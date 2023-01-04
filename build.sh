a5d5e3cbdea9f0b8b505effca8508815983c9497
Grégory Libert <greg_libert@yahoo.fr>
2023-01-04 11:50:19 +0100

    renames main go file and update build.sh to match the new name (#27)

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ PLUGIN=wallet-plugin
 
 # Build the binary
 mkdir -p "build/$PLUGIN/"
-CGO_ENABLED="1" go build -o build/$PLUGIN/thyra-plugin-wallet  cmd/massa-wallet/main.go
+CGO_ENABLED="1" go build -o build/$PLUGIN/thyra-plugin-wallet  cmd/massa-wallet/thyra-plugin-wallet.go
