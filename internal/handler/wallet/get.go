6b9cfbafbc0f498b2e39953561ce65a0abc71495
noesusset <noe.susset@epita.fr>
2023-02-08 22:03:11 +0100

    fix: fixed the list of wallets request

--- internal/handler/wallet/get.go
+++ internal/handler/wallet/get.go
@@ -1,6 +1,7 @@
 package wallet
 
 import (
+	"github.com/btcsuite/btcutil/base58"
 	"github.com/go-openapi/runtime/middleware"
 
 	"github.com/massalabs/thyra-plugin-wallet/api/server/models"
@@ -23,11 +24,20 @@ func HandleList(params operations.RestWalletListParams) middleware.Responder {
 	var wlts []*models.Wallet
 
 	for i := 0; i < len(wallets); i++ {
+		privK := base58.CheckEncode(wallets[i].KeyPair.PrivateKey, wallet.Base58Version)
+		pubK := base58.CheckEncode(wallets[i].KeyPair.PublicKey, wallet.Base58Version)
+		salt := base58.CheckEncode(wallets[i].KeyPair.Salt[:], wallet.Base58Version)
+		nonce := base58.CheckEncode(wallets[i].KeyPair.Nonce[:], wallet.Base58Version)
 		wlts = append(wlts,
 			&models.Wallet{
 				Nickname: wallets[i].Nickname,
 				Address:  wallets[i].Address,
-				KeyPair:  models.WalletKeyPair{},
+				KeyPair:  models.WalletKeyPair{
+					PrivateKey: privK,
+					PublicKey:  pubK,
+					Salt:       salt,
+					Nonce:      nonce,
+				},
 			})
 	}
 
