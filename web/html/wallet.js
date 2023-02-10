a99e008f6b1e841c8254082d410da9b26e485c4f
Sebastien Valla <sv@massa.net>
2023-02-10 15:56:47 +0100

    add balance

--- web/html/wallet.js
+++ web/html/wallet.js
@@ -12,7 +12,7 @@ function openNickNameModal() {
 
 function closeModal() {
     $("#nicknameModal").modal("hide");
-    document.getElementById("nicknameInput").value = ""
+    document.getElementById("nicknameInput").value = "";
 }
 
 function closeModalOnClickOn(elementID) {
@@ -31,7 +31,7 @@ async function importWallet() {
             wallets.push(resp.data);
         })
         .catch(handleAPIError);
-        closeModal()
+    closeModal();
 }
 
 // Create a wallet through POST query
@@ -67,7 +67,17 @@ function createWallet() {
         .catch(handleAPIError);
 }
 
-function tableInsert(resp) {
+// Fetch a wallet's balance through POST query
+async function fetchBalanceOf(addresses) {
+    const getBalance = await axios.post("http://my.massa/massa/addresses", {
+        addresses: addresses,
+        options: ["balances"],
+    });
+
+    return getBalance.data.pendingBalances;
+}
+
+async function tableInsert(resp) {
     const tBody = document
         .getElementById("user-wallet-table")
         .getElementsByTagName("tbody")[0];
@@ -79,8 +89,11 @@ function tableInsert(resp) {
     const cell3 = row.insertCell();
 
     cell0.innerHTML = addressInnerHTML(resp.address);
+
     cell1.innerHTML = resp.nickname;
-    cell2.innerHTML = 0;
+
+    const balance = await fetchBalanceOf([resp.address]);
+    cell2.innerHTML = parseFloat(balance[0]);
     cell3.innerHTML =
         '<svg class="quit-button" onclick="deleteRow(this)" xmlns="http://www.w3.org/2000/svg" width="24" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line> <line x1="6" y1="6" x2="18" y2="18"></line></svg>';
 }
