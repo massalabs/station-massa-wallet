733bce33761c7b94f6be7435e21027d951318203
Sebastien Valla <sv@massa.net>
2023-02-10 16:02:38 +0100

    add balance

--- web/html/wallet.js
+++ web/html/wallet.js
@@ -1,37 +1,37 @@
-document.getElementById("import-wallet").addEventListener("click", openDialog);
-document.getElementById("fileid").addEventListener("change", handleFileSelect, true);
+document
+    .getElementById("import-wallet")
+    .addEventListener("click", openNickNameModal);
 
+closeModalOnClickOn("close-button");
+closeModalOnClickOn("nicknameCancelBtn");
 getWallets();
+
+function openNickNameModal() {
+    $("#nicknameModal").modal("show");
+}
+
+function closeModal() {
+    $("#nicknameModal").modal("hide");
+    document.getElementById("nicknameInput").value = ""
+}
+
+function closeModalOnClickOn(elementID) {
+    document.getElementById(elementID).addEventListener("click", closeModal);
+}
 
 let wallets = [];
 
-// open file upload
-function openDialog() {
-    document.getElementById("fileid").value = null;
-    document.getElementById("fileid").click();
-}
-
-// Handle event on file selecting
-function handleFileSelect(evt) {
-    let files = evt.target.files; // get files
-    let f = files[0];
-    const reader = new FileReader();
-    reader.onload = (event) => importWallet(JSON.parse(event.target.result)); // desired file content
-    reader.onerror = (error) => reject(error);
-    reader.readAsText(f);
-}
-
 // Import a wallet through PUT query
-async function importWallet(wallet) {
+async function importWallet() {
+    let nickname = document.getElementById("nicknameInput").value;
     axios
-        .put("/rest/wallet", wallet)
-        .then((_) => {
-            tableInsert(wallet);
-            wallets.push(wallet);
+        .post(`/rest/wallet/import/${nickname}`)
+        .then((resp) => {
+            tableInsert(resp.data);
+            wallets.push(resp.data);
         })
-        .catch((e) => {
-            errorAlert(getErrorMessage(e.response.data.code));
-        });
+        .catch(handleAPIError);
+    closeModal()
 }
 
 // Create a wallet through POST query
@@ -47,10 +47,7 @@ async function getWallets() {
                 wallets = data;
             }
         })
-        .catch((e) => {
-            console.error(e);
-            errorAlert(getErrorMessage(e.response.data.code));
-        });
+        .catch(handleAPIError);
 }
 
 // Create a wallet through POST query
@@ -67,13 +64,23 @@ function createWallet() {
             tableInsert(resp.data);
             wallets.push(resp.data);
         })
-        .catch((e) => {
-            errorAlert(getErrorMessage(e.response.data.code));
-        });
+        .catch(handleAPIError);
 }
 
-function tableInsert(resp) {
-    const tBody = document.getElementById("user-wallet-table").getElementsByTagName("tbody")[0];
+// Fetch a wallet's balance through POST query
+async function fetchBalanceOf(addresses) {
+    const getBalance = await axios.post("http://my.massa/massa/addresses", {
+        addresses: addresses,
+        options: ["balances"],
+    });
+
+    return getBalance.data.pendingBalances;
+}
+
+async function tableInsert(resp) {
+    const tBody = document
+        .getElementById("user-wallet-table")
+        .getElementsByTagName("tbody")[0];
     const row = tBody.insertRow(-1);
 
     const cell0 = row.insertCell();
@@ -82,8 +89,11 @@ function tableInsert(resp) {
     const cell3 = row.insertCell();
 
     cell0.innerHTML = addressInnerHTML(resp.address);
+
     cell1.innerHTML = resp.nickname;
-    cell2.innerHTML = 0;
+
+    const balance = await fetchBalanceOf([resp.address]);
+    cell2.innerHTML = parseFloat(balance[0]);
     cell3.innerHTML =
         '<svg class="quit-button" onclick="deleteRow(this)" xmlns="http://www.w3.org/2000/svg" width="24" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line> <line x1="6" y1="6" x2="18" y2="18"></line></svg>';
 }
@@ -91,7 +101,9 @@ function tableInsert(resp) {
 function deleteRow(element) {
     const rowIndex = element.parentNode.parentNode.rowIndex;
 
-    const tBody = document.getElementById("user-wallet-table").getElementsByTagName("tbody")[0];
+    const tBody = document
+        .getElementById("user-wallet-table")
+        .getElementsByTagName("tbody")[0];
     const nickname = tBody.rows[rowIndex - 1].cells[1].innerHTML;
 
     axios
@@ -99,9 +111,7 @@ function deleteRow(element) {
         .then((_) => {
             wallets = wallets.filter((wallet) => wallet.nickname != nickname);
         })
-        .catch((e) => {
-            errorAlert(getErrorMessage(e.response.data.code));
-        });
+        .catch(handleAPIError);
 
     document.getElementById("user-wallet-table").deleteRow(rowIndex);
 }
