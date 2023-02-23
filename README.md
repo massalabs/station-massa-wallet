73a436a9d1bf6b402d96dea535fddd696287c5ea
Nathan Seva <thykof@protonmail.ch>
2023-02-23 12:17:18 +0100

    register the plugin

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Massa wallet plugin
-This is the thyra plugin that implements the Massa wallet features.
+
+This is the Thyra plugin that implements the Massa wallet features.
 
 ## How to test it?
+
 The first thing to do is to find the endpoint that the plugin listens to.
 
 When you start this plugin, which you can do using the command `go run cmd/massa-wallet/main.go` for example, you will get a message like the following in the terminal:
@@ -12,10 +14,37 @@ When you start this plugin, which you can do using the command `go run cmd/massa
 
 The listening port is at the end of the first line. In this example, the port is `33049`.
 
-You can then access the service using the following URL: http://127.0.0.1:33049
+You can then access the service using the following URL: <http://127.0.0.1:33049>
 
 ### Postman collection
 
 You will find a postman collection in the `/api` directory.
 
 Before testing this API, you must initialize the `baseURL` variable to 127.0.0.1:`port`, port being the listening port that the plugin listens to (`33049` in the previous example).
+
+## Contribute
+
+Generate:
+
+```shell
+go generate ./...
+```
+
+Run:
+
+```shell
+go run cmd/massa-wallet/thyra-plugin-wallet.go 1 --standalone
+```
+
+Build:
+
+```shell
+go build -o wallet ./cmd/massa-wallet/thyra-plugin-wallet.go
+```
+
+Install the plugin:
+
+```shell
+mkdir ~/.config/thyra/my_plugins/wallet
+mv wallet ~/.config/thyra/my_plugins/wallet
+```
--- cmd/massa-wallet/thyra-plugin-wallet.go
+++ cmd/massa-wallet/thyra-plugin-wallet.go
@@ -1,36 +1,33 @@
 package main
 
 import (
-	"flag"
 	"log"
+	"net"
+	"os"
+	"strconv"
+	"strings"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"github.com/massalabs/thyra-plugin-wallet/api/server/restapi"
 	"github.com/massalabs/thyra-plugin-wallet/internal/handler"
 	"github.com/massalabs/thyra-plugin-wallet/pkg/password"
+	"github.com/massalabs/thyra-plugin-wallet/pkg/plugin"
 	"github.com/massalabs/thyra-plugin-wallet/pkg/privateKey"
 )
 
 func main() {
-	var port int
-	var path string
+	ln, _ := net.Listen("tcp", ":")
 
-	flag.IntVar(&port, "port", 8080, "the port to listen on")
-
-	// The path is not actually used in the script.
-	// It is included only to maintain temporary compatibility with Thyra, and it will be removed at a later time.
-	flag.StringVar(&path, "path", "", "the path to listen on")
-
-	flag.Parse()
+	registerPlugin(ln)
 
 	myApp := app.New()
-	go StartServer(&myApp, port)
+	go startServer(&myApp, ln)
 
 	myApp.Run()
 }
 
-func StartServer(app *fyne.App, port int) {
+func startServer(app *fyne.App, ln net.Listener) {
 	//mandatory to free main thread
 	defer (*app).Quit()
 
@@ -45,10 +42,32 @@ func StartServer(app *fyne.App, port int) {
 	server.ConfigureAPI()
 
 	// Set the port to listen on to the passed-in port
+	port, _ := strconv.Atoi(strings.Split(ln.Addr().String(), ":")[1])
 	server.Port = port
 
 	if err := server.Serve(); err != nil {
-
 		log.Fatalln(err)
+	}
+}
+
+func registerPlugin(ln net.Listener) {
+	//nolint:gomnd
+	if len(os.Args) < 2 {
+		panic("this program must be run with correlation id argument!")
+	}
+
+	pluginID := os.Args[1]
+
+	standaloneMode := false
+
+	if len(os.Args) == 3 && os.Args[2] == "--standalone" {
+		standaloneMode = true
+	}
+
+	if !standaloneMode {
+		err := plugin.Register(pluginID, "Massa Wallet", "Massalabs", "Massa wallet for Thyra", ln.Addr())
+		if err != nil {
+			log.Panicln(err)
+		}
 	}
 }
