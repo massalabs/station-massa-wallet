747a495919ab1e28b725a1722755af53a045aa99
Nathan Seva <thykof@protonmail.ch>
2023-03-13 11:09:38 +0100

    allow CORS

--- api/server/restapi/configure_massa_wallet.go
+++ api/server/restapi/configure_massa_wallet.go
@@ -10,6 +10,7 @@ import (
 	"github.com/go-openapi/errors"
 	"github.com/go-openapi/runtime"
 	"github.com/go-openapi/runtime/middleware"
+	"github.com/rs/cors"
 
 	"github.com/massalabs/thyra-plugin-wallet/api/server/restapi/operations"
 )
@@ -115,5 +116,7 @@ func setupMiddlewares(handler http.Handler) http.Handler {
 // The middleware configuration happens before anything, this middleware also applies to serving the swagger.json document.
 // So this is a good place to plug in a panic handling middleware, logging and metrics.
 func setupGlobalMiddleware(handler http.Handler) http.Handler {
-	return handler
+	handleCORS := cors.Default().Handler
+
+	return handleCORS(handler)
 }
