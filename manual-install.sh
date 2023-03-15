237a004ef8dc1d19d0a1a4ea8fefc34dce0dc961
Nathan Seva <thykof@protonmail.ch>
2023-03-15 10:49:47 +0100

    fixes CD

---
+++ manual-install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -ex
+
+PLUGIN=wallet-plugin
+
+mkdir -p ~/.config/thyra/my_plugins/thyra-plugin-wallet
+mv build/$PLUGIN/thyra-plugin-wallet ~/.config/thyra/my_plugins/thyra-plugin-wallet
