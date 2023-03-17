367b9157bc2fc70472cbf7cc94ec734061ca2895
MaZ <126461030+mazmassa@users.noreply.github.com>
2023-03-17 17:56:34 +0100

    remove unreacheble code from delete wallet (#57)

--- internal/handler/wallet/delete.go
+++ internal/handler/wallet/delete.go
@@ -11,14 +11,6 @@ import (
 
 // HandleDelete handles a delete request
 func HandleDelete(params operations.RestWalletDeleteParams) middleware.Responder {
-	if len(params.Nickname) == 0 {
-		return operations.NewRestWalletDeleteBadRequest().WithPayload(
-			&models.Error{
-				Code:    errorDeleteNoNickname,
-				Message: "Error: nickname field is mandatory.",
-			})
-	}
-
 	err := wallet.Delete(params.Nickname)
 	if err != nil {
 		return operations.NewRestWalletDeleteInternalServerError().WithPayload(
