7460ba8e0be0c1ad9f94016357fc9772eb70c3d4
Nathan Seva <thykof@protonmail.ch>
2023-04-11 17:03:09 +0200

    rename plugin directory

--- manual-install.sh
+++ manual-install.sh
@@ -2,5 +2,5 @@
 
 PLUGIN=wallet-plugin
 
-mkdir -p ~/.config/thyra/my_plugins/thyra-plugin-wallet
-cp build/$PLUGIN/thyra-plugin-wallet ~/.config/thyra/my_plugins/thyra-plugin-wallet
+mkdir -p ~/.config/thyra/plugins/thyra-plugin-wallet
+cp build/$PLUGIN/thyra-plugin-wallet ~/.config/thyra/plugins/thyra-plugin-wallet
