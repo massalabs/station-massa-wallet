76206d0ab4a3c9b33d164ff88982d420fbe3e691
Nathan Seva <thykof@protonmail.ch>
2023-04-26 00:43:16 +0200

    use tailwind preset

--- web-frontend/tailwind.config.js
+++ web-frontend/tailwind.config.js
@@ -1,19 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'landing-page': "url('./src/assets/bg-image-landing-page.svg')",
-      },
-    },
-    colors: {
-      'bg-primary': '#151A26',
-      green: '#1AE19D',
-    },
-    letterSpacing: {
-      massa: '-0.01em',
-    },
-  },
-  plugins: [],
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    './node_modules/@massalabs/react-ui-kit/src/**/*.{js,ts,jsx,tsx}',
+  ],
+  presets: [require('@massalabs/react-ui-kit/presets/massa-station-preset.js')],
 };
