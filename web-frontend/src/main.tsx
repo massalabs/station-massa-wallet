60b2bfc3452135ac39c3d8b4d40ba5c2e6d067c5
Nathan Seva <thykof@protonmail.ch>
2023-05-03 17:38:56 +0200

    Fix react router 404

--- web-frontend/src/main.tsx
+++ web-frontend/src/main.tsx
@@ -5,12 +5,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import '@massalabs/react-ui-kit/src/global.css';
 import Welcome from './pages/Welcome.tsx';
+import Error from './pages/Error.tsx';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter basename={import.meta.env.VITE_BASE_PATH}>
       <Routes>
         <Route path="index" element={<Welcome />} />
+        <Route path="*" element={<Error />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
--- web-frontend/src/pages/Error.tsx
+++ web-frontend/src/pages/Error.tsx
@@ -1,21 +1,11 @@
-import { useRouteError } from 'react-router-dom';
-
-interface ErrorRoute {
-  statusText: string;
-  message: string;
-}
+import { Link } from 'react-router-dom';
 
 export default function Error() {
-  const error = useRouteError() as ErrorRoute;
-  console.error(error);
-
   return (
     <div id="error-page">
       <h1 className="mas-banner">Oops!</h1>
       <p className="mas-body">Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
+      <Link to="index">Go back to the Welcome page!</Link>
     </div>
   );
 }
