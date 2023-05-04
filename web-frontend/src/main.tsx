1125f8ddb28a3816e36fe213701c8a46a07be3db
Nathan Seva <thykof@protonmail.ch>
2023-05-04 12:03:21 +0200

    improve react router routes declaration

--- web-frontend/src/main.tsx
+++ web-frontend/src/main.tsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from 'react-router-dom';
 
 import './index.css';
 import '@massalabs/react-ui-kit/src/global.css';
 import Welcome from './pages/Welcome.tsx';
 import Error from './pages/Error.tsx';
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path={import.meta.env.VITE_BASE_PATH}>
+      <Route path="index" element={<Welcome />} />
+      <Route path="*" element={<Error />} />
+    </Route>,
+  ),
+);
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.VITE_BASE_PATH}>
-      <Routes>
-        <Route path="index" element={<Welcome />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
-    </BrowserRouter>
+    <RouterProvider router={router} fallbackElement={<Error />} />
   </React.StrictMode>,
 );
