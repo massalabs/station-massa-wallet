72d6337f6efb32666966e61eef254d2eaebe707a
Nathan Seva <thykof@protonmail.ch>
2023-05-10 11:45:41 +0200

    update manual install script

--- manual-install.sh
+++ manual-install.sh
@@ -4,3 +4,5 @@ PLUGIN=wallet-plugin
 
 mkdir -p ~/.config/thyra/plugins/wallet-plugin
 cp build/$PLUGIN/wallet-plugin ~/.config/thyra/plugins/wallet-plugin
+cp manifest.json ~/.config/thyra/plugins/wallet-plugin
+cp web/html/wallet.svg ~/.config/thyra/plugins/wallet-plugin
