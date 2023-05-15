4540e1d0e66f9d598ee375416b59afae5bed6e88
Maxime Carabina <maxime.carabina@epitech.eu>
2023-05-15 15:10:32 +0200

    Setup Tailwind in wails front-end

--- wails-frontend/src/main.tsx
+++ wails-frontend/src/main.tsx
@@ -6,6 +6,7 @@ import {
   createRoutesFromElements,
   RouterProvider,
 } from 'react-router-dom';
+import './index.css';
 
 import App from './app';
 import PasswordPrompt from './pages/passwordPrompt';
