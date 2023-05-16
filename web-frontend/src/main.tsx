0002c8737cfb44880aad6bd9ab7b9471fba446c2
Pivi Lartisant <90157528+pivilartisant@users.noreply.github.com>
2023-05-16 11:32:51 +0200

    add screen 3 for account creation (#201)

--- web-frontend/src/main.tsx
+++ web-frontend/src/main.tsx
@@ -20,6 +20,7 @@ if (import.meta.env.VITE_ENV === 'dev') {
   mockServer();
 }
 import StepOne from './pages/CreateAccount/StepOne.tsx';
+import StepThree from './pages/CreateAccount/StepThree.tsx';
 
 const queryClient = new QueryClient();
 
@@ -33,6 +34,7 @@ const router = createBrowserRouter(
       <Route path="error" element={<Error />} />
       <Route path="account-new" element={<AddAccount />} />
       <Route path="account-create-step1" element={<StepOne />} />
+      <Route path="account-create-step-three" element={<StepThree />} />
       <Route path="*" element={<Error />} />
     </Route>,
   ),
