094c90e5798097d1ab618ef4c32d7e15fe6084ea
Pierre GUÉVENEUX <83134981+Pierregvx@users.noreply.github.com>
2023-05-16 14:14:47 +0200

    185 refactor export test (#187)
    * refacto export_test
    
    * fix build error

--- internal/handler/wallet/export_test.go
+++ internal/handler/wallet/export_test.go
@@ -3,29 +3,22 @@ package wallet
 import (
 	"fmt"
 	"net/http/httptest"
-	"strings"
 	"testing"
 
 	"github.com/massalabs/thyra-plugin-wallet/pkg/wallet"
+	"github.com/stretchr/testify/assert"
 )
 
 func Test_exportFileWallet_handler(t *testing.T) {
 	api, _, _, err := MockAPI()
-	if err != nil {
-		panic(err)
-	}
+	assert.NoError(t, err)
 
 	handler, exist := api.HandlerFor("get", "/api/accounts/{nickname}/exportFile")
-	if !exist {
-		panic("Endpoint doesn't exist")
-	}
-
+	assert.True(t, exist)
 	// test empty configuration first.
 	t.Run("Export file of unknown wallet", func(t *testing.T) {
 		resp, err := handleHTTPRequest(handler, "GET", fmt.Sprintf("/api/accounts/%s/exportFile", "nobody"), "")
-		if err != nil {
-			t.Fatalf("while serving HTTP request: %s", err)
-		}
+		assert.NoError(t, err)
 
 		verifyStatusCode(t, resp, 404)
 	})
@@ -35,14 +28,10 @@ func Test_exportFileWallet_handler(t *testing.T) {
 		nickname := "trololol"
 		password := "zePassword"
 		_, err = wallet.Generate(nickname, password)
-		if err != nil {
-			t.Fatalf(err.Error())
-		}
+		assert.NoError(t, err)
 
 		resp, err := handleHTTPRequest(handler, "GET", fmt.Sprintf("/api/accounts/%s/exportFile", nickname), "")
-		if err != nil {
-			t.Fatalf("while serving HTTP request: %s", err)
-		}
+		assert.NoError(t, err)
 
 		verifyStatusCode(t, resp, 200)
 		verifyHeader(t, resp, "Content-Type", "application/octet-stream")
@@ -50,26 +39,19 @@ func Test_exportFileWallet_handler(t *testing.T) {
 		verifyBodyWalletBackup(t, resp, nickname)
 
 		err = cleanupTestData([]string{nickname})
-		if err != nil {
-			t.Fatalf("while cleaning up TestData: %s", err)
-		}
+		assert.NoError(t, err)
 	})
 }
 
 func verifyHeader(t *testing.T, resp *httptest.ResponseRecorder, headerName, headerValue string) {
-	if resp.Header().Get(headerName) != headerValue {
-		t.Fatalf("the header %s was: %s, want %s", headerName, resp.Header().Get(headerName), headerValue)
-	}
+
+	assert.Equal(t, resp.Header().Get(headerName), headerValue)
 }
 
 func verifyBodyWalletBackup(t *testing.T, resp *httptest.ResponseRecorder, nickname string) {
 	body := resp.Body.String()
-	if body == "" {
-		t.Fatalf("the body was empty")
-	}
+	assert.NotEmpty(t, body)
 
 	// check the first line
-	if !strings.Contains(body, "Nickname: "+nickname) {
-		t.Fatalf("the body doesn't contain the wallet nickname")
-	}
+	assert.Contains(t, body, "Nickname: "+nickname)
 }
