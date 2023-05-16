141718a81cc3f0f78cf13b8cb8b5da0282692589
pivi <paul.lartisant@icloud.com>
2023-05-16 12:13:31 +0200

    Add Wails Import Screen 1

--- wails-frontend/src/pages/importFile.tsx
+++ wails-frontend/src/pages/importFile.tsx
@@ -9,6 +9,7 @@ import { useLocation, useNavigate } from 'react-router-dom';
 import { handleApplyResult, handleCancel } from '../utils/utils';
 import { walletapp } from '../../wailsjs/go/models';
 import { EventsOnce } from '../../wailsjs/runtime/runtime';
+import { Button } from '@massalabs/react-ui-kit';
 
 const ImportFile = () => {
   const nav = useNavigate();
@@ -21,11 +22,15 @@ const ImportFile = () => {
   const { state } = useLocation();
   const req: promptRequest = state.req;
 
+  const accountStyleSuccess = ' mas-body text-s-success  pb-4';
+  const accountStyleNormal = ' mas-body text-neutral  pb-4';
+
   const baselineStr = () =>
     account
-      ? `Importing account ${account.nickname}`
+      ? `Selected ${account.nickname}'s account`
       : 'Select an account file to import';
-  const applyStr = () => (account ? 'Import' : 'Select a file');
+
+  const importStr = () => (account ? 'Import' : 'Select a file');
 
   const handleApply = async () => {
     if (!account) {
@@ -34,7 +39,8 @@ const ImportFile = () => {
         return;
       }
       if (res.err) {
-        setErrorMsg(res.err);
+        // setErrorMsg(res.err);
+        setErrorMsg('The file format must be .yaml');
         return;
       }
       setAccount(res);
@@ -48,19 +54,31 @@ const ImportFile = () => {
   };
 
   return (
-    <section>
-      <div>{req.Msg}</div>
-      <div className="baseline">{baselineStr()}</div>
-      <div className="flex">
-        <button className="btn" onClick={handleCancel}>
-          Cancel
-        </button>
-        <button className="btn" onClick={handleApply}>
-          {applyStr()}
-        </button>
+    <div className="bg-primary flex flex-col justify-center items-center h-screen w-full">
+      <div className="w-1/4  max-w-sm  min-w-fit">
+        <div>
+          <p className="mas-title text-neutral pb-4">{req.Msg}</p>
+        </div>
+        <div>
+          <p className={account ? accountStyleSuccess : accountStyleNormal}>
+            {baselineStr()}
+          </p>
+        </div>
+        <div className="flex flex-row gap-4  pb-4">
+          <div className="min-w-fit">
+            <Button variant={'secondary'} onClick={handleCancel}>
+              Cancel
+            </Button>
+          </div>
+          <div className="min-w-fit">
+            <Button onClick={handleApply}>{importStr()}</Button>
+          </div>
+        </div>
+        <div>
+          <p className="mas-body text-s-error">{errorMsg}</p>
+        </div>
       </div>
-      <div className="baseline">{errorMsg}</div>
-    </section>
+    </div>
   );
 };
 
