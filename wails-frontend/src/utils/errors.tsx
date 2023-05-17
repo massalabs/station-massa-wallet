019a0188e8e376dfbf755d14562b852acf5fb131
Nathan Seva <thykof@protonmail.ch>
2023-05-17 16:40:16 +0200

    wails: format code

--- wails-frontend/src/utils/errors.tsx
+++ wails-frontend/src/utils/errors.tsx
@@ -1,4 +1,4 @@
-export const errorsEN:  Record<string, string> = {
+export const errorsEN: Record<string, string> = {
   'Nickname-0001': 'Invalid nickname',
   'PrivateKey-0001': 'Invalid private key',
   'AccountFile-0001': 'Filesystem error',
