eb8f2a858d5587035222760d3a2565edd9b33cd8
Maxime Carabina <72019005+maxime-carabina@users.noreply.github.com>
2023-05-22 13:29:58 +0200

    wails: add default layout (#227)

---
+++ wails-frontend/src/layouts/Layout/Layout.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from 'react';
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+export function Layout(props: LayoutProps) {
+  return (
+    <div className="bg-primary min-h-screen">
+      <div
+        className="flex flex-col justify-center h-screen
+        max-w-xs min-w-fit text-f-primary m-auto"
+      >
+        {props.children}
+      </div>
+    </div>
+  );
+}
