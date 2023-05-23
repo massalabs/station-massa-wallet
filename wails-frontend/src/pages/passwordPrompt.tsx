3554064f3259d0c0b1846c4329593339b98ffff9
Maxime Carabina <maxime.carabina@epitech.eu>
2023-05-23 10:31:09 +0200

    revemp form password

--- wails-frontend/src/pages/passwordPrompt.tsx
+++ wails-frontend/src/pages/passwordPrompt.tsx
@@ -1,5 +1,7 @@
 /* eslint-disable new-cap */
-import { useState } from 'react';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+import { useState, useRef, SyntheticEvent } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import {
   ApplyPassword,
@@ -8,12 +10,15 @@ import {
 import { EventsOnce } from '../../wailsjs/runtime';
 import { events, promptAction, promptRequest } from '../events/events';
 import { handleApplyResult, handleCancel } from '../utils/utils';
+import { parseForm } from '../utils/parseForm';
+import { hasMoreThanFiveChars, hasSamePassword } from '../validation/password';
 
 import { FiLock } from 'react-icons/fi';
-import { Password, Button } from '@massalabs/react-ui-kit';
+import { Input, Button } from '@massalabs/react-ui-kit';
 
 function PasswordPrompt() {
   const navigate = useNavigate();
+  const form = useRef(null);
 
   const { state } = useLocation();
   const req: promptRequest = state.req;
@@ -21,12 +26,6 @@ function PasswordPrompt() {
   const { newPasswordReq, importReq, deleteReq } = promptAction;
   const isNewPassword = req.Action === newPasswordReq;
   const isImport = req.Action === importReq;
-
-  const [resultMsg, setResultMsg] = useState('');
-  const [password, setPassword] = useState('');
-  const [passwordConfirm, setPasswordConf] = useState<string | undefined>(
-    undefined,
-  );
 
   function getButtonLabel() {
     switch (req.Action) {
@@ -52,78 +51,82 @@ function PasswordPrompt() {
     }
   }
 
-  function isDisabled() {
-    if (isNewPassword || isImport) {
-      return !(password && passwordConfirm);
+  const [error, setError] = useState<[string | undefined]>(undefined);
+
+  function validate(e: SyntheticEvent) {
+    const form = parseForm(e);
+    const { password, passwordConfirm } = form;
+
+    if (!hasMoreThanFiveChars(password)) {
+      setError({ password: 'Password must have at least 5 characters' });
+      return false;
+    } else if (!hasSamePassword(password, passwordConfirm)) {
+      setError({ password: "Passwords don't match" });
+      return false;
     }
-    return !password;
+    return true;
   }
 
-  function applyPassword() {
-    if (password.length) {
-      if (passwordConfirm && password !== passwordConfirm) {
-        setResultMsg("Passwords don't match");
-        return;
-      }
-      EventsOnce(
-        events.passwordResult,
-        handleApplyResult(
-          navigate,
-          req,
-          setResultMsg,
-          state.req.Action === promptAction.importReq,
-        ),
-      );
+  function save(e: SyntheticEvent) {
+    const form = parseForm(e);
+    const { password } = form;
 
-      if (state.req.Action === promptAction.importReq) {
-        return ImportPrivateKey(state.pkey, state.nickname, password);
-      }
-      return ApplyPassword(password);
+    EventsOnce(
+      events.passwordResult,
+      handleApplyResult(navigate, req, setError, isImport),
+    );
+
+    return isImport
+      ? ImportPrivateKey(state.pkey, state.nickname, password)
+      : ApplyPassword(password);
+  }
+
+  async function handleSubmit(e: SyntheticEvent) {
+    e.preventDefault();
+
+    if (validate(e)) {
+      save(e);
     }
   }
 
   return (
     <div className="bg-primary min-h-screen">
-      <div
-        className="flex flex-col justify-center h-screen
-        max-w-xs min-w-fit text-f-primary m-auto"
-      >
-        <h1 className="mas-title">{req.Msg}</h1>
-        <p className="mas-body pt-4">{getSubtitle()}</p>
-        <div className="pt-4">
-          <Password
-            onChange={(e) => setPassword(e.target.value)}
-            value={password}
-            name="input"
-            placeholder="Enter your password"
-          />
-        </div>
-        {(isNewPassword || isImport) && (
+      <form ref={form} onSubmit={handleSubmit}>
+        <div
+          className="flex flex-col justify-center h-screen
+          max-w-xs min-w-fit text-f-primary m-auto"
+        >
+          <h1 className="mas-title">{req.Msg}</h1>
+          <p className="mas-body pt-4">{getSubtitle()}</p>
           <div className="pt-4">
-            <Password
-              onChange={(e) => setPasswordConf(e.target.value)}
-              value={passwordConfirm}
-              name="input"
-              placeholder="Confirm your password"
+            <Input
+              defaultValue=""
+              name="password"
+              placeholder="Enter your password"
+              error={error?.password}
             />
           </div>
-        )}
-        <p className="pt-4 mas-body">{resultMsg}</p>
-        <div className="pt-4 flex gap-4">
-          <div className="max-w-min">
-            <Button variant={'secondary'} onClick={handleCancel}>
-              Cancel
+          {(isNewPassword || isImport) && (
+            <div className="pt-4">
+              <Input
+                defaultValue=""
+                name="passwordConfirm"
+                placeholder="Confirm your password"
+              />
+            </div>
+          )}
+          <div className="pt-4 flex gap-4">
+            <div className="max-w-min">
+              <Button variant={'secondary'} onClick={handleCancel}>
+                Cancel
+              </Button>
+            </div>
+            <Button preIcon={<FiLock />} type="submit">
+              {getButtonLabel()}
             </Button>
           </div>
-          <Button
-            preIcon={<FiLock />}
-            onClick={applyPassword}
-            disabled={isDisabled()}
-          >
-            {getButtonLabel()}
-          </Button>
         </div>
-      </div>
+      </form>
     </div>
   );
 }
