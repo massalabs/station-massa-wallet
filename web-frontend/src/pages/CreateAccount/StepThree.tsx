9c50d41a8f51a13a55ec47f3e5af4ae1b29c275e
Maxime Carabina <maxime.carabina@epitech.eu>
2023-05-26 18:00:14 +0200

    web: update step3 and add account

--- web-frontend/src/pages/CreateAccount/StepThree.tsx
+++ web-frontend/src/pages/CreateAccount/StepThree.tsx
@@ -1,34 +1,41 @@
-import { Stepper } from '@massalabs/react-ui-kit/src/components/Stepper/Stepper';
-import { Button } from '@massalabs/react-ui-kit/src/components/Button/Button';
+import { Link } from 'react-router-dom';
+import Intl from '../../i18n/i18n';
+import { routeFor } from '../../utils';
+
+import { Button, Stepper } from '@massalabs/react-ui-kit';
 import { FiArrowRight } from 'react-icons/fi';
 import LandingPage from '../../layouts/LandingPage/LandingPage';
 
 export default function StepThree() {
   return (
     <LandingPage>
-      <div
-        className={`flex flex-col justify-center items-center align-center h-screen`}
-      >
-        <div className="flex flex-col justify-center items-center w-fit h-fit max-w-sm">
-          <div className=" w-full max-w-xs mb-6">
-            <Stepper step={2} steps={['Username', 'Password', 'Backup']} />
+      <div className="flex flex-col justify-center items-center align-center h-screen">
+        <div className="flex flex-col justify-center items-center w-fit h-fit max-w-sm text-neutral">
+          <div className="w-full max-w-xs">
+            <Stepper
+              step={2}
+              steps={[
+                Intl.t('account.create.step1.title'),
+                Intl.t('account.create.step2.title'),
+                Intl.t('account.create.step3.title'),
+              ]}
+            />
           </div>
-          <div className="w-full">
-            <h1 className="mas-banner text-neutral mb-6">Create an account</h1>
+          <h1 className="mas-banner pt-6">{Intl.t('account.create.title')}</h1>
+          <p className="mas-body pt-6">
+            {Intl.t('account.create.step3.description')}
+          </p>
+          <div className="pt-4 w-full">
+            <Link to={routeFor('homepage')}>
+              <Button posIcon={<FiArrowRight />}>
+                {Intl.t('account.create.buttons.skip')}
+              </Button>
+            </Link>
           </div>
-          <div className="w-full mb-4">
-            <p className="mas-body text-neutral">
-              Security is very important, we recommend backing up your account
-              using a key pair or a .yaml file.
-              <br />
-              Would you like to do it now or later?
-            </p>
-          </div>
-          <div className="w-full mb-4">
-            <Button posIcon={<FiArrowRight className="h-6 w-6" />}>Skip</Button>
-          </div>
-          <div className="w-full">
-            <Button variant="secondary">Save now</Button>
+          <div className="pt-4 w-full">
+            <Button variant="secondary">
+              {Intl.t('account.create.buttons.backup')}
+            </Button>
           </div>
         </div>
       </div>
