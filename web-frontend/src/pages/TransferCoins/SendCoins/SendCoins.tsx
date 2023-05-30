1b99968ec72b24f5944634135b6c642b57963397
pivi <paul.lartisant@icloud.com>
2023-05-30 12:31:50 +0200

    create backend skeleton

--- web-frontend/src/pages/TransferCoins/SendCoins/SendCoins.tsx
+++ web-frontend/src/pages/TransferCoins/SendCoins/SendCoins.tsx
@@ -1,6 +1,22 @@
-function SendCoins() {
-  // This is a placeholder
-  return <h1>Send coins </h1>;
-}
+import { useState } from 'react';
 
-export default SendCoins;
+export function SendCoins() {
+  const [amount, setAmount] = useState<number>(64);
+  const [fees, setFees] = useState<number>(42);
+  const [recepient, setRecepient] = useState<number>(42);
+  return (
+    <div className="bg-primary w-full h-screen">
+      <h1>Send coins </h1>
+
+      <form className="flex flex-col w-1/2">
+        <input type="text" value={amount} placeholder="amount"></input>
+        <input type="text" value={fees} placeholder="fees"></input>
+        <input
+          type="text"
+          value={recepient}
+          placeholder="recepient address"
+        ></input>
+      </form>
+    </div>
+  );
+}
