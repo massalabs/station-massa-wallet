98223a7214bdacd25224a3bf2a617cfb62de1dd1
pivi <paul.lartisant@icloud.com>
2023-05-31 19:52:14 +0200

    fix input bug + refactor

--- web-frontend/src/pages/TransferCoins/SendCoins/Send.tsx
+++ web-frontend/src/pages/TransferCoins/SendCoins/Send.tsx
@@ -20,25 +20,31 @@ function Send() {
   const nickname = state.nickname;
   const { data: accounts = [] } = useResource<AccountObject[]>('accounts');
   const account = accounts.find((account) => account.nickname === nickname);
-  const balance = parseInt(account!.candidateBalance);
-  const [amount, setAmount] = useState<number>(0);
+  const balance = parseInt(account?.candidateBalance ?? '0') / 10 ** 9;
+  const [amount, setAmount] = useState<string>('');
   const [error, setError] = useState<IErrorObject | null>(null);
   const [recipient, setRecipient] = useState('');
+
   const selectPercentClass = 'mr-3.5 hover:cursor-pointer';
 
-  // refactor to switch => todo
-  // Check to see if address exists in our system
+  // TODO : implement address check to see if it is real
 
   function validate(e: any) {
     const formObject = parseForm(e);
     const { amount, recipient } = formObject;
 
-    if (!amount) {
+    const amountNum = reverseFormatStandard(amount);
+    console.log(amount, amountNum, balance);
+
+    if (!amountNum && amount == '') {
       setError({ amount: 'Amount is required' });
       return false;
     }
-    const int = parseInt(amount);
-    if (Number.isNaN(int)) {
+    if (amountNum > balance) {
+      setError({ amount: 'Insufficient funds on balance' });
+      return false;
+    }
+    if (Number.isNaN(amountNum)) {
       setError({ amount: 'Please enter a valid number' });
       return false;
     }
@@ -51,35 +57,41 @@ function Send() {
   }
 
   // This function is going to be used for the backend
+  // Some parts are commented to avoid a build errors
+  // because the amount and recipient aren't used yet
   function handleSubmit(e: any) {
     e.preventDefault();
     if (!validate(e)) return;
-
-    const formObject = parseForm(e);
-
-    const { amount, recipient } = formObject;
-
-    // console logs to avoid build fail
-    console.log(amount);
-    console.log(recipient);
-    console.log('submit');
+    // const formObject = parseForm(e);
+    // const { amount, recipient } = formObject;
   }
-  function formatStandard(num: number) {
+
+  function formatStandard(num: number, maximumFractionDigits = 2) {
     return num.toLocaleString('en-US', {
       minimumFractionDigits: 2,
-      maximumFractionDigits: 2,
+      maximumFractionDigits,
     });
   }
+
+  function reverseFormatStandard(str: string) {
+    const formattedString = str.replace(/[^0-9.-]/g, ''); // Remove non-numeric characters
+    return parseFloat(formattedString);
+  }
+
   const formattedBalance = formatStandard(balance);
 
   function overrideAmount(pct: number) {
-    setAmount(balance * pct);
+    const newAmount = balance * pct;
+    const newFormatedAmount = formatStandard(newAmount, 9);
+    setAmount(newFormatedAmount);
   }
 
   function SendPercentage({ percentage }: { percentage: number }) {
     return (
       <li
-        onClick={() => overrideAmount(percentage / 100)}
+        onClick={() => {
+          overrideAmount(percentage / 100);
+        }}
         className={selectPercentClass}
       >
         {percentage == 100 ? 'Max' : `${percentage}%`}
@@ -87,64 +99,9 @@ function Send() {
     );
   }
 
-  // JSX form
-  function AmountSection() {
-    return (
-      <div className="mb-3.5">
-        <div className="flex flex-row justify-between w-full mb-3.5 ">
-          <p>Send Token</p>
-          <p>
-            Available : <u>{formattedBalance}</u>
-          </p>
-        </div>
-        <div className="mb-3.5">
-          <Input
-            placeholder={'Amount to send'}
-            // value={parseToStandard(parseInt(amount ? amount : '0'))!}
-            value={formatStandard(amount)}
-            name="amount"
-            onChange={(e) => setAmount(parseInt(e.target.value))}
-            error={error?.amount}
-          />
-        </div>
-        <div className="flex flex-row-reverse">
-          <ul className="flex flex-row">
-            <SendPercentage percentage={25}></SendPercentage>
-            <SendPercentage percentage={50}></SendPercentage>
-            <SendPercentage percentage={75}></SendPercentage>
-            <SendPercentage percentage={100}></SendPercentage>
-          </ul>
-        </div>
-      </div>
-    );
-  }
-
-  function RecipientSection() {
-    return (
-      <div>
-        <div className="mb-3.5">
-          <p>To :</p>
-        </div>
-        <div className="mb-3.5">
-          <Input
-            placeholder={'Recipient'}
-            value={recipient}
-            onChange={(e) => setRecipient(e.target.value)}
-            icon={<FiUsers />}
-            name="recipient"
-            error={error?.recipient}
-          />
-        </div>
-        <div className="flex flex-row-reverse mb-3.5">
-          <p
-            className="hover:cursor-pointer"
-            onClick={() => console.log('transfer between accounts')}
-          >
-            <u>Transfer between my account</u>
-          </p>
-        </div>
-      </div>
-    );
+  function handleChange(e: any) {
+    e.preventDefault();
+    setAmount(e.target.value);
   }
 
   return (
@@ -154,19 +111,67 @@ function Send() {
         <div>
           <p> Account Balance</p>
           <Balance
-            className="pl-0"
+            customClass="pl-0"
             amount={formattedBalance}
-            equal={'$0012345.67'}
+            equal={'0012345.67'}
           />
         </div>
-        <AmountSection />
-        <RecipientSection />
+        <div className="mb-3.5">
+          <div className="flex flex-row justify-between w-full mb-3.5 ">
+            <p>Send Token</p>
+            <p>
+              Available : <u>{formattedBalance}</u>
+            </p>
+          </div>
+          <div className="mb-3.5">
+            <Input
+              placeholder={'Amount to send'}
+              value={amount}
+              name="amount"
+              onChange={(e) => handleChange(e)}
+              error={error?.amount}
+            />
+          </div>
+          <div className="flex flex-row-reverse">
+            <ul className="flex flex-row">
+              <SendPercentage percentage={25} />
+              <SendPercentage percentage={50} />
+              <SendPercentage percentage={75} />
+              <SendPercentage percentage={100} />
+            </ul>
+          </div>
+        </div>
+        <div>
+          <div className="mb-3.5">
+            <p>To :</p>
+          </div>
+          <div className="mb-3.5">
+            <Input
+              placeholder={'Recipient'}
+              value={recipient}
+              onChange={(e) => setRecipient(e.target.value)}
+              icon={<FiUsers />}
+              name="recipient"
+              error={error?.recipient}
+            />
+          </div>
+          <div className="flex flex-row-reverse mb-3.5">
+            <p
+              className="hover:cursor-pointer"
+              onClick={() => console.log('transfer between accounts')}
+            >
+              <u>Transfer between my account</u>
+            </p>
+          </div>
+        </div>
         {/* Button Section */}
         <div className="flex flex-col w-full">
-          <Button variant={'secondary'}>Advanced</Button>
-          {/* Temp fix for buttons not taking full width*/}
-          <div className="mb-3.5"></div>
-          <Button type="submit"> Send </Button>
+          <div className="mb-3.5">
+            <Button variant={'secondary'}>Advanced</Button>
+          </div>
+          <div>
+            <Button type="submit"> Send </Button>
+          </div>
         </div>
       </form>
     </div>
