5a90e58bf5bc68ccd14f53d1a205644d66f9cebb
Maxime Carabina <72019005+maxime-carabina@users.noreply.github.com>
2023-06-02 16:59:56 +0200

    web: fix menu item selection (#298) (#303)
    Co-authored-by: Nathan Seva <thykof@protonmail.ch>

--- web-frontend/src/pages/Settings/Settings.tsx
+++ web-frontend/src/pages/Settings/Settings.tsx
@@ -1,12 +1,14 @@
+import { useState } from 'react';
 import { useLocation } from 'react-router-dom';
-import { usePost, useDelete } from '../../custom/api';
+import { usePost, useDelete, usePut } from '../../custom/api';
 import { AccountObject } from '../../models/AccountModel';
+import Intl from '../../i18n/i18n';
 
 import WalletLayout, {
   MenuItem,
 } from '../../layouts/WalletLayout/WalletLayout';
 
-import { Button } from '@massalabs/react-ui-kit';
+import { Button, Identicon, Input } from '@massalabs/react-ui-kit';
 import { FiTrash2 } from 'react-icons/fi';
 
 export default function Settings() {
@@ -21,21 +23,56 @@ export default function Settings() {
     `accounts/${nickname}`,
   );
 
+  const { mutate: mutableUpdate } = usePut<AccountObject>(
+    `accounts/${nickname}`,
+  );
+
+  const [newNickname, setNewNickname] = useState<string>(nickname);
+
   return (
     <WalletLayout menuItem={MenuItem.Settings}>
-      <div className="bg-secondary rounded-2xl w-full max-w-2xl p-10 m-auto">
-        <p className="mas-body text-f-primary">Account security</p>
-        <div className="flex gap-5 pt-5">
-          <Button onClick={() => mutableBackup({} as AccountObject)}>
-            Back up account
-          </Button>
+      <div className="flex flex-col justify-center items-center gap-9">
+        <div className="bg-secondary rounded-2xl w-full max-w-2xl p-10">
+          <p className="mas-body text-f-primary pb-5">
+            {Intl.t('settings.title-profil')}
+          </p>
+          <div className="pb-7">
+            <Identicon
+              username={newNickname}
+              size={120}
+              className="bg-primary rounded-full"
+            />
+          </div>
+          <div className="pb-5">
+            <Input
+              value={newNickname}
+              onChange={(e) => setNewNickname(e.target.value)}
+            />
+          </div>
           <Button
-            preIcon={<FiTrash2 />}
-            onClick={() => mutableDelete({} as AccountObject)}
-            variant="danger"
+            onClick={() =>
+              mutableUpdate({ nickname: newNickname } as AccountObject)
+            }
           >
-            Delete account
+            {Intl.t('settings.buttons.update')}
           </Button>
+        </div>
+        <div className="bg-secondary rounded-2xl w-full max-w-2xl p-10">
+          <p className="mas-body text-f-primary pb-5">
+            {Intl.t('settings.title-security')}
+          </p>
+          <div className="flex gap-5">
+            <Button onClick={() => mutableBackup({} as AccountObject)}>
+              {Intl.t('settings.buttons.backup')}
+            </Button>
+            <Button
+              preIcon={<FiTrash2 />}
+              onClick={() => mutableDelete({} as AccountObject)}
+              variant="danger"
+            >
+              {Intl.t('settings.buttons.delete')}
+            </Button>
+          </div>
         </div>
       </div>
     </WalletLayout>
