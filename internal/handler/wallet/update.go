27c26dcefcd823b64adb1c86478e35a7487a67c8
Nathan Seva <thykof@protonmail.ch>
2023-06-05 12:26:45 +0200

    300 implement update nickname (#308)
    * implement update account
    
    * add update account tests
    
    * update postman collection
    
    * int: rename edit account
    
    * refactor: improve delete file

---
+++ internal/handler/wallet/update.go
@@ -0,0 +1,85 @@
+package wallet
+
+import (
+	"fmt"
+
+	"github.com/go-openapi/runtime/middleware"
+
+	"github.com/massalabs/thyra-plugin-wallet/api/server/models"
+	"github.com/massalabs/thyra-plugin-wallet/api/server/restapi/operations"
+	"github.com/massalabs/thyra-plugin-wallet/pkg/network"
+	"github.com/massalabs/thyra-plugin-wallet/pkg/prompt"
+	"github.com/massalabs/thyra-plugin-wallet/pkg/utils"
+	"github.com/massalabs/thyra-plugin-wallet/pkg/wallet"
+)
+
+type PromptRequestEditAccountData struct {
+	Nickname string
+	Balance  string
+}
+
+func NewEditAccount(prompterApp prompt.WalletPrompterInterface, massaClient network.NodeFetcherInterface) operations.EditAccountHandler {
+	return &walletEditAccount{prompterApp: prompterApp, massaClient: massaClient}
+}
+
+type walletEditAccount struct {
+	prompterApp prompt.WalletPrompterInterface
+	massaClient network.NodeFetcherInterface
+}
+
+// HandleDelete handles a delete request
+func (m *walletEditAccount) Handle(params operations.EditAccountParams) middleware.Responder {
+	wlt, resp := loadWallet(params.Nickname)
+	if resp != nil {
+		return resp
+	}
+
+	newWlt, errModify := m.handleEditAccount(wlt, params.Body.NewNickname)
+	if errModify != nil {
+		return operations.NewGetAccountInternalServerError().WithPayload(
+			&models.Error{
+				Code:    errModify.CodeErr,
+				Message: errModify.Err.Error(),
+			})
+	}
+
+	modelWallet := createModelWallet(*newWlt)
+
+	infos, err := m.massaClient.GetAccountsInfos([]wallet.Wallet{*wlt})
+	if err != nil {
+		return operations.NewGetAccountInternalServerError().WithPayload(
+			&models.Error{
+				Code:    errorGetWallets,
+				Message: "Unable to retrieve account infos",
+			})
+	}
+
+	modelWallet.CandidateBalance = models.Amount(fmt.Sprint(infos[0].CandidateBalance))
+	modelWallet.Balance = models.Amount(fmt.Sprint(infos[0].Balance))
+
+	return operations.NewGetAccountOK().WithPayload(&modelWallet)
+}
+
+func (m *walletEditAccount) handleEditAccount(wlt *wallet.Wallet, newNickname models.Nickname) (*wallet.Wallet, *wallet.WalletError) {
+	oldNickname := wlt.Nickname
+
+	// persist new nickname before deleting old file
+	wlt.Nickname = string(newNickname)
+	err := wlt.Persist()
+	if err != nil {
+		return nil, &wallet.WalletError{
+			Err:     fmt.Errorf("persisting the modified account: %w", err),
+			CodeErr: utils.ErrAccountFile,
+		}
+	}
+
+	// delete old file
+	if wallet.DeleteAccount(oldNickname) != nil {
+		return nil, &wallet.WalletError{
+			Err:     fmt.Errorf("persisting the old account: %w", err),
+			CodeErr: utils.ErrAccountFile,
+		}
+	}
+
+	return wlt, nil
+}
---
+++ internal/handler/wallet/update_test.go
@@ -0,0 +1,68 @@
+package wallet
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/massalabs/thyra-plugin-wallet/api/server/models"
+	"github.com/massalabs/thyra-plugin-wallet/pkg/wallet"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_ModifyWallets_handler(t *testing.T) {
+	api, _, _, err := MockAPI()
+	assert.NoError(t, err)
+
+	// Create account
+	nickname := "trololol-old"
+	password := "zePassword"
+	_, errGenerate := wallet.Generate(nickname, password)
+	assert.Nil(t, errGenerate)
+
+	handler, exist := api.HandlerFor("put", "/api/accounts/{nickname}")
+	assert.True(t, exist, "Endpoint doesn't exist")
+
+	newNickname := "toby-new"
+	payload := fmt.Sprintf(`{
+		"newNickname": "%s"
+	}`, newNickname)
+
+	t.Run("update invalid payload", func(t *testing.T) {
+		resp, err := handleHTTPRequest(handler, "PUT", fmt.Sprintf("/api/accounts/%s", nickname), "")
+		assert.NoError(t, err)
+		assert.Equal(t, http.StatusUnprocessableEntity, resp.Result().StatusCode)
+	})
+
+	t.Run("update unknown account", func(t *testing.T) {
+		resp, err := handleHTTPRequest(handler, "PUT", fmt.Sprintf("/api/accounts/%s", "bhabhabha"), payload)
+		assert.NoError(t, err)
+		assert.Equal(t, http.StatusNotFound, resp.Result().StatusCode)
+	})
+
+	t.Run("update account", func(t *testing.T) {
+		resp, err := handleHTTPRequest(handler, "PUT", fmt.Sprintf("/api/accounts/%s", nickname), payload)
+		assert.NoError(t, err)
+		assert.Equal(t, http.StatusOK, resp.Result().StatusCode)
+
+		assertWalletBody(t, resp, newNickname, true)
+
+		// Check that the old nickname doesn't exist anymore
+		resp, err = processHTTPRequest(api, "GET", "/api/accounts", "")
+		assert.NoError(t, err)
+		assert.Equal(t, resp.Result().StatusCode, http.StatusOK)
+
+		var wallets []models.Account
+		err = json.Unmarshal(resp.Body.Bytes(), &wallets)
+		assert.NoError(t, err)
+
+		assert.Len(t, wallets, 1, "there should be only one wallet")
+		assert.Equal(t, wallets[0].Nickname, models.Nickname(newNickname), "the nickname should have been updated")
+
+		// Clean account file
+		err = cleanupTestData([]string{newNickname})
+		assert.NoError(t, err, fmt.Sprintf("while cleaning up TestData: %s", err))
+	})
+}
