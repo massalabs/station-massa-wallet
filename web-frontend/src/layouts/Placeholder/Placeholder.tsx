fb8ef5b80927a6e1df180efbd9ca992dd1d903b4
pivi <paul.lartisant@icloud.com>
2023-06-06 16:49:12 +0200

    Add INterface For Placeholder

--- web-frontend/src/layouts/Placeholder/Placeholder.tsx
+++ web-frontend/src/layouts/Placeholder/Placeholder.tsx
@@ -1,17 +1,22 @@
 import Intl from '../../i18n/i18n';
 
-function Placeholder({ ...props }) {
+interface IPlaceholder {
+  message: string;
+  icon: JSX.Element;
+}
+
+function Placeholder(props: IPlaceholder) {
   const { message, icon } = props;
   return (
     <div
       className="flex flex-col justify-around items-center  
-        h-96 w-[700px] p-10 gap-10
+        h-96 max-w-2xl p-10 gap-10
         relative left-20
         bg-secondary rounded-lg"
     >
       {icon}
       <h1 className="mas-banner text-center">
-        {Intl.t('placeholder.placeholder-banner')}
+        {Intl.t('placeholder.teaser-banner')}
       </h1>
       <p className="mas-buttons text-center">{message}</p>
     </div>
