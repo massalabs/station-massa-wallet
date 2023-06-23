b3a966ee30ff800c76b293cb769ba31384be4082
Nathan Seva <thykof@protonmail.ch>
2023-06-23 12:50:59 +0200

    various improvements

--- web-frontend/src/pages/Error.tsx
+++ web-frontend/src/pages/Error.tsx
@@ -1,12 +1,25 @@
 import { Link } from 'react-router-dom';
 import Intl from '../i18n/i18n';
+import { routeFor } from '../utils';
+import LandingPage from '../layouts/LandingPage/LandingPage';
 
 export default function Error() {
   return (
-    <div id="error-page">
-      <h1 className="mas-banner">{Intl.t('unexpected-error.title')}</h1>
-      <p className="mas-body">{Intl.t('unexpected-error.description')}</p>
-      <Link to="index">{Intl.t('unexpected-error.link')}</Link>
-    </div>
+    <LandingPage>
+      <div
+        id="error-page"
+        className="flex flex-col justify-center items-center h-screen text-f-primary"
+      >
+        <h1 className="mas-banner">
+          {Intl.t('errors.unexpected-error.title')}
+        </h1>
+        <p className="mas-bod">
+          {Intl.t('errors.unexpected-error.description')}
+        </p>
+        <Link to={routeFor('index')} className="underline">
+          {Intl.t('errors.unexpected-error.link')}
+        </Link>
+      </div>
+    </LandingPage>
   );
 }
