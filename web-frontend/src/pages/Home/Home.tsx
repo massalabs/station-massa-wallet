45d31e4f79f7cc7a3dcd4148af86ddbc51b4a6c1
pivi <paul.lartisant@icloud.com>
2023-06-30 17:30:31 +0200

    Revert "Add Dynamic Address Render"
    This reverts commit 3aa803e555038392a4c2387adbfd58372c74a864.

--- web-frontend/src/pages/Home/Home.tsx
+++ web-frontend/src/pages/Home/Home.tsx
@@ -1,7 +1,7 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { AccountObject } from '../../models/AccountModel';
-import { formatStandard, Unit, maskAddress } from '../../utils/massaFormat';
+import { formatStandard, Unit } from '../../utils/massaFormat';
 import { useResource } from '../../custom/api';
 import { routeFor } from '../../utils';
 import Intl from '../../i18n/i18n';
@@ -23,22 +23,6 @@ export default function Home() {
     isLoading,
   } = useResource<AccountObject>(`accounts/${nickname}`);
 
-  const breakpoint = 1920; // Screen breakpoint value
-  const unformattedBalance = account?.candidateBalance ?? '0';
-  const balance = parseInt(unformattedBalance);
-  const formattedBalance = formatStandard(balance, Unit.NanoMAS);
-  const address = account?.address ?? '';
-  const formattedAddress = maskAddress(address);
-
-  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
-  const [displayedContent, setDisplayedContent] = useState(formattedAddress);
-
-  // Function seems unnecessary,
-  // but it is needed to update the state of screenWidth
-  function handleResize() {
-    setScreenWidth(window.innerWidth);
-  }
-
   useEffect(() => {
     if (error) {
       navigate(routeFor('error'));
@@ -47,20 +31,10 @@ export default function Home() {
     }
   }, [account, navigate]);
 
-  // Call handleResize once after component mounts to log formattedAddress on page load
-  useEffect(() => {
-    window.addEventListener('resize', handleResize);
-    handleResize();
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
-
-  useEffect(() => {
-    screenWidth <= breakpoint
-      ? setDisplayedContent(formattedAddress)
-      : setDisplayedContent(address);
-  }, [screenWidth, breakpoint, address, formattedAddress]);
+  const unformattedBalance = account?.candidateBalance ?? '0';
+  const balance = parseInt(unformattedBalance);
+  const formattedBalance = formatStandard(balance, Unit.NanoMAS);
+  const address = account?.address ?? '';
 
   return (
     <WalletLayout menuItem={MenuItem.Home}>
@@ -103,7 +77,7 @@ export default function Home() {
               {Intl.t('home.title-account-address')}
             </p>
             <Clipboard
-              displayedContent={displayedContent}
+              displayedContent={address}
               rawContent={address}
               error={Intl.t('errors.no-content-to-copy')}
               className="flex flex-row items-center mas-body2 justify-between
