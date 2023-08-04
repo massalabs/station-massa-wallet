a2f0f27d9a30ed92810ef365d6f64675ae406d84
BRAHIMI Naoufal <nb@massa.net>
2023-08-04 11:55:46 +0200

    CR : move coode, refactor tests

--- internal/handler/wallet/add_assets_test.go
+++ internal/handler/wallet/add_assets_test.go
@@ -4,88 +4,121 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
 	"testing"
 
 	"github.com/massalabs/station-massa-wallet/api/server/models"
+	"github.com/massalabs/station-massa-wallet/api/server/restapi/operations"
 	utils "github.com/massalabs/station-massa-wallet/pkg/assets"
 	"github.com/massalabs/station-massa-wallet/pkg/wallet"
+	"github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestAddAssetHandler_InvalidAddress(t *testing.T) {
+func TestAddAssetHandler(t *testing.T) {
 	api, _, _, _, err := MockAPI()
 	assert.NoError(t, err)
 
 	nickname := "GoodNickname"
-	invalidAddress := "InvalidAddress"
 	password := "zePassword"
 
 	_, errGenerate := wallet.Generate(nickname, password)
 	assert.Nil(t, errGenerate)
 
-	// Get the handler for the AddAsset endpoint
-	handler, exist := api.HandlerFor("post", "/api/accounts/{nickname}/assets")
-	assert.True(t, exist)
+	t.Run("InvalidAddress", func(t *testing.T) {
+		invalidAddress := "InvalidAddress"
+		// Get the handler for the AddAsset endpoint
+		handler, exist := api.HandlerFor("post", "/api/accounts/{nickname}/assets")
+		assert.True(t, exist)
 
-	// Send the POST request with an invalid address
-	resp, err := handleHTTPRequest(handler, "POST", fmt.Sprintf("/api/accounts/%s/assets?assetAddress=%s", nickname, invalidAddress), "")
-	assert.NoError(t, err)
+		// Send the POST request with an invalid address
+		resp, err := handleHTTPRequest(handler, "POST", fmt.Sprintf("/api/accounts/%s/assets?assetAddress=%s", nickname, invalidAddress), "")
+		assert.NoError(t, err)
 
-	// Assert that the response status code is 422 Unprocessable Entity
-	assert.Equal(t, http.StatusUnprocessableEntity, resp.Result().StatusCode)
+		// Assert that the response status code is 422 Unprocessable Entity
+		assert.Equal(t, http.StatusUnprocessableEntity, resp.Result().StatusCode)
 
-	// Parse the error response from the body
-	var errorResponse models.Error
-	_ = json.Unmarshal(resp.Body.Bytes(), &errorResponse)
-	// Assert that the error message match the expected values
-	assert.Equal(t, "assetAddress in query should match '^AS[0-9a-zA-Z]+$'", errorResponse.Message)
+		// Parse the error response from the body
+		var errorResponse models.Error
+		_ = json.Unmarshal(resp.Body.Bytes(), &errorResponse)
+		// Assert that the error message matches the expected values
+		assert.Equal(t, "assetAddress in query should match '^AS[0-9a-zA-Z]+$'", errorResponse.Message)
 
-	// Remove the json file created
-	err = utils.RemoveJSONFile()
-	assert.NoError(t, err)
+		// Remove the json file created
+		err = RemoveJSONFile()
+		assert.NoError(t, err)
+	})
 
-	err = cleanupTestData([]string{nickname})
-	assert.NoError(t, err)
+	t.Run("AssetAlreadyExists", func(t *testing.T) {
+		existingAddress := "AS17gQyPvtwGQ2rfvE6L91J3N7ebvnvsSuh44vADVrPSFVW3vw96"
+
+		// Get the handler for the AddAsset endpoint
+		handler, exist := api.HandlerFor("post", "/api/accounts/{nickname}/assets")
+		assert.True(t, exist)
+
+		// Add the asset with the existing address for the first time
+		_ = addAssetTest(t, api, nickname, existingAddress)
+
+		// Attempt to add the asset with the existing address again
+		resp, err := handleHTTPRequest(handler, "POST", fmt.Sprintf("/api/accounts/%s/assets?assetAddress=%s", nickname, existingAddress), "")
+		assert.NoError(t, err)
+
+		// Assert that the response status code is 400 Bad Request
+		assert.Equal(t, http.StatusBadRequest, resp.Result().StatusCode)
+
+		// Parse the error response from the body
+		var errorResponse models.Error
+		err = json.Unmarshal(resp.Body.Bytes(), &errorResponse)
+		assert.NoError(t, err)
+
+		// Assert that the error message matches the expected value
+		assert.Equal(t, "Asset with the provided address already exists.", errorResponse.Message)
+
+		// Remove the json file created
+		err = RemoveJSONFile()
+		assert.NoError(t, err)
+
+		err = cleanupTestData([]string{nickname})
+		assert.NoError(t, err)
+	})
 }
 
-func TestAddAssetHandler_AssetAlreadyExists(t *testing.T) {
-	api, _, _, _, err := MockAPI()
-	assert.NoError(t, err)
+func addAssetTest(t *testing.T, api *operations.MassaWalletAPI, nickname, assetAddress string) *models.AssetInfo {
+	t.Helper()
 
-	nickname := "GoodNickname"
-	existingAddress := "AS17gQyPvtwGQ2rfvE6L91J3N7ebvnvsSuh44vADVrPSFVW3vw96"
-
-	password := "zePassword"
-
-	_, errGenerate := wallet.Generate(nickname, password)
-	assert.Nil(t, errGenerate)
-
-	// Get the handler for the AddAsset endpoint
 	handler, exist := api.HandlerFor("post", "/api/accounts/{nickname}/assets")
 	assert.True(t, exist)
 
-	// Add the asset with the existing address for the first time
-	_ = addAssetTest(t, api, nickname, existingAddress)
-
-	// Attempt to add the asset with the existing address again
-	resp, err := handleHTTPRequest(handler, "POST", fmt.Sprintf("/api/accounts/%s/assets?assetAddress=%s", nickname, existingAddress), "")
+	// Send the POST request with a valid address to add a new asset
+	resp, err := handleHTTPRequest(handler, "POST", fmt.Sprintf("/api/accounts/%s/assets?assetAddress=%s", nickname, assetAddress), "")
 	assert.NoError(t, err)
 
-	// Assert that the response status code is 400 Bad Request
-	assert.Equal(t, http.StatusBadRequest, resp.Result().StatusCode)
+	assert.Equal(t, http.StatusCreated, resp.Result().StatusCode)
 
-	// Parse the error response from the body
-	var errorResponse models.Error
-	err = json.Unmarshal(resp.Body.Bytes(), &errorResponse)
+	// Parse the response body to get the added asset
+	var addedAsset models.AssetInfo
+	err = json.Unmarshal(resp.Body.Bytes(), &addedAsset)
 	assert.NoError(t, err)
 
-	// Assert that the error message matches the expected value
-	assert.Equal(t, "Asset with the provided address already exists.", errorResponse.Message)
+	return &addedAsset
+}
 
-	// Remove the json file created
-	err = utils.RemoveJSONFile()
-	assert.NoError(t, err)
+// RemoveJSONFile removes the assets.json file if it exists.
+func RemoveJSONFile() error {
+	assetsJSONPath, err := utils.GetAssetsJSONPath()
+	if err != nil {
+		return err
+	}
 
-	err = cleanupTestData([]string{nickname})
-	assert.NoError(t, err)
+	if _, err := os.Stat(assetsJSONPath); os.IsNotExist(err) {
+		// The file does not exist, so there's nothing to remove.
+		return nil
+	}
+
+	// The file exists, so let's attempt to remove it.
+	if err := os.Remove(assetsJSONPath); err != nil {
+		return errors.Wrap(err, "failed to remove assets JSON file")
+	}
+
+	return nil
 }
---
+++ internal/handler/wallet/delete_asset_test.go
@@ -0,0 +1,80 @@
+package wallet
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/massalabs/station-massa-wallet/api/server/restapi/operations"
+	"github.com/massalabs/station-massa-wallet/pkg/wallet"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAddGetDeleteAsset(t *testing.T) {
+	api, _, _, _, err := MockAPI()
+	assert.NoError(t, err)
+
+	nickname := "GoodNickname"
+	password := "zePassword"
+
+	_, errGenerate := wallet.Generate(nickname, password)
+	assert.Nil(t, errGenerate)
+
+	// Create the test wallet first
+
+	// Step 1: Get the assets before adding
+	assetsBeforeAdd := getAssets(t, api, nickname)
+
+	// Step 2: Add a new asset to the wallet
+	validAddress := "AS17gQyPvtwGQ2rfvE6L91J3N7ebvnvsSuh44vADVrPSFVW3vw96"
+	addedAsset := addAssetTest(t, api, nickname, validAddress)
+
+	// Step 3: Get the assets again after adding
+	assetsAfterAdd := getAssets(t, api, nickname)
+
+	// Assert that the number of assets increased by 1 after adding
+	assert.Len(t, assetsAfterAdd, len(assetsBeforeAdd)+1, "the assets list should have increased by 1 after adding")
+
+	// Assert the first asset remains unchanged
+	assertAssetInfoWithBalanceEqual(t, assetsAfterAdd[0], assetsBeforeAdd[0])
+
+	// Assert the newly added asset
+	assert.Equal(t, "TestToken", addedAsset.Name)
+	assert.Equal(t, "TST", addedAsset.Symbol)
+	assert.Equal(t, int64(9), *addedAsset.Decimals)
+
+	// Assert the balance of the newly added asset
+	assert.Equal(t, "1000000", assetsAfterAdd[0].Balance)
+
+	// Step 4: Delete the imported asset
+	deleteAssetTest(t, api, nickname, validAddress)
+
+	// Step 5: Get the assets again after deletion
+	assetsAfterDelete := getAssets(t, api, nickname)
+
+	// Assert that the number of assets is back to the original count after deletion
+	assert.Len(t, assetsAfterDelete, len(assetsBeforeAdd), "the assets list should be back to the original count after deletion")
+
+	// Assert the first asset remains unchanged after deletion
+	assertAssetInfoWithBalanceEqual(t, assetsAfterDelete[0], assetsBeforeAdd[0])
+
+	// Remove the json file created
+	err = RemoveJSONFile()
+	assert.NoError(t, err)
+
+	err = cleanupTestData([]string{nickname})
+	assert.NoError(t, err)
+}
+
+func deleteAssetTest(t *testing.T, api *operations.MassaWalletAPI, nickname, assetAddress string) {
+	t.Helper()
+
+	handler, exist := api.HandlerFor("delete", "/api/accounts/{nickname}/assets")
+	assert.True(t, exist)
+
+	// Send the DELETE request to delete the asset
+	resp, err := handleHTTPRequest(handler, "DELETE", fmt.Sprintf("/api/accounts/%s/assets?assetAddress=%s", nickname, assetAddress), "")
+	assert.NoError(t, err)
+
+	assert.Equal(t, http.StatusCreated, resp.Result().StatusCode)
+}
