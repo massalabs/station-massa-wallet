36fec6d0660b01cdaacf42461501b8ba4d517561
BRAHIMI Naoufal <nb@massa.net>
2023-09-27 15:36:40 +0200

    add unit tests for utils and openapi

--- pkg/utils/amounts_test.go
+++ pkg/utils/amounts_test.go
@@ -29,3 +29,25 @@ func Test_MasToNano(t *testing.T) {
 		assert.Equal(t, test.input, mas)
 	}
 }
+
+func Test_NanoToMas(t *testing.T) {
+	tests := []struct {
+		input uint64
+		want  string
+	}{
+		{uint64(0), "0"},
+		{math.MaxUint64, "18446744073.709551615"},
+		{uint64(123456789), "0.123456789"},
+		{uint64(123456789000000000), "123456789"},
+		{uint64(123456789123456789), "123456789.123456789"},
+		{uint64(1), "0.000000001"},
+		{uint64(999999999999999999), "999999999.999999999"},
+	}
+
+	for _, test := range tests {
+		mas, err := NanoToMas(test.input)
+		if assert.NoError(t, err, "Expected no error for NanoToMas conversion of %d", test.input) {
+			assert.Equal(t, test.want, mas, "NanoToMas conversion for input: %d", test.input)
+		}
+	}
+}
