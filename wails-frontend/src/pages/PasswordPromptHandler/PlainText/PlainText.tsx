31b0c2d005bd53a36b7bff64ff7ee7b62cda88c2
pivi <paul.lartisant@icloud.com>
2023-10-10 10:45:02 +0200

    fix plaintext

--- wails-frontend/src/pages/PasswordPromptHandler/PlainText/PlainText.tsx
+++ wails-frontend/src/pages/PasswordPromptHandler/PlainText/PlainText.tsx
@@ -37,7 +37,7 @@ export function PlainText(props: PromptRequestData) {
               isChild={false}
               iconOpen={<FiChevronDown />}
               iconClose={<FiChevronUp />}
-              customClass={'!p-0'}
+              customClass={'px-0 py-0'}
               categoryTitle={
                 <div className="flex items-center w-full gap-4">
                   <FiInfo size={18} />
