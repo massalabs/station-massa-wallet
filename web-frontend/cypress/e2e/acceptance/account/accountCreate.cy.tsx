f14e8b8a740be54816730ecdbfc8b17e4ab312e6
pivi <paul.lartisant@icloud.com>
2023-10-17 16:20:59 +0200

    WIP e2e test

--- web-frontend/cypress/e2e/acceptance/account/accountCreate.cy.tsx
+++ web-frontend/cypress/e2e/acceptance/account/accountCreate.cy.tsx
@@ -11,38 +11,23 @@ describe('E2E | Acceptance | create', function () {
       cy.get('[data-testid="account-select-list"]').should('exist');
 
       cy.get('[data-testid="account-create"]').should('exist');
+
+      cy.get('[data-testid="account-select-list-0"]').should('exist');
     });
-
-    cy.get('[data-testid="account-select-list-0"]').should('exist');
   });
+  //   it('should display accounts list after loading', () => {
+  //     cy.visit(`/`).then(() => {
+  //       cy.url().should('include', '/account-select');
+
+  //       cy.intercept(
+  //         {
+  //           method: 'GET',
+  //           url: `${Cypress.config().baseUrl}/accounts`,
+  //         },
+  //         { fixture: 'accounts.json' },
+  //       ).as('getAccounts');
+
+  //       cy.wait('@getAccounts');
+  //     });
+  //   });
 });
-
-// it('should display accounts list after loading', () => {
-//   cy.visit(`/`).then(() => {
-//     cy.url().should('include', '/account-select');
-
-//     cy.intercept(
-//       {
-//         method: 'GET',
-//         url: `**/accounts`,
-//       },
-//       { fixture: 'accounts.json' },
-//     ).as('getAccounts');
-
-//     cy.wait('@getAccounts').then((interception) => {
-//       // const request = interception.request;
-//       // const response = interception.response;
-//       // console.log('request', request);
-//       // console.log('response', response);
-
-//       cy.get('[data-testid="account-select-page"]').should('exist');
-
-//       //   cy.get('[data-testid="account-select-list"]').should('exist');
-//       //   cy.get('[data-testid="account-create"]').should('exist');
-
-//       //   cy.get('[data-testid="account-select-list"]').should('exist');
-//       //   cy.get('[data-testid="account-select-list-0"]').should('exist');
-//       // });
-//     });
-//   });
-// });
