6afa8a21b083eb2fdfce648b415d690e30e56a87
Nathan Seva <thykof@protonmail.ch>
2023-10-18 13:31:51 -0500

    move constants

--- pkg/utils/errors.go
+++ pkg/utils/errors.go
@@ -7,6 +7,31 @@ import (
 	"github.com/massalabs/station-massa-wallet/pkg/walletmanager"
 )
 
+// Error codes
+const (
+	ErrInvalidNickname   = "Nickname-0001"
+	ErrInvalidPrivateKey = "PrivateKey-0001"
+	ErrAccountFile       = "AccountFile-0001"
+	ErrNoFile            = "NoFile-0001"
+	ErrDuplicateKey      = "DuplicateKey-0001"
+	ErrUnknown           = "Unknown-0001"
+	ErrDuplicateNickname = "DuplicateNickname-001"
+	ErrTimeoutMsg        = "Timeout-0001"
+	ErrNetwork           = "Network-0001"
+	ErrPromptInputType   = "InvalidPromptInput-0001"
+)
+
+// Message codes
+const (
+	WrongPassword  = "WrongPassword-0001"
+	ActionCanceled = "ActionCanceled-0001"
+)
+
+// Messages
+const (
+	MsgTransferRequest = "transfer-request"
+)
+
 // Sentinel errors
 var (
 	ErrCorrelationIDNotFound = errors.New("Correlation ID not found")
