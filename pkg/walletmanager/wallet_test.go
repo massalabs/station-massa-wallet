470afbeb703faf73e6c21965a94edcb022e90e79
Grégory Libert <gl@massa.net>
2023-10-19 09:06:46 +0200

    Fix code coverage duplicate code warning

--- pkg/walletmanager/wallet_test.go
+++ pkg/walletmanager/wallet_test.go
@@ -22,35 +22,38 @@ func TestWallet(t *testing.T) {
 	assert.NoError(t, err)
 
 	var w *Wallet
-	sampleSalt := [16]byte{145, 114, 211, 33, 247, 163, 215, 171, 90, 186, 97, 47, 43, 252, 68, 170}
-	sampleNonce := [12]byte{113, 122, 168, 123, 48, 187, 178, 12, 209, 91, 243, 63}
-	sampleNickname := "bonjour2"
-	sampleAccount, err := account.New(
+	defaultSalt := [16]byte{145, 114, 211, 33, 247, 163, 215, 171, 90, 186, 97, 47, 43, 252, 68, 170}
+	defaultNonce := [12]byte{113, 122, 168, 123, 48, 187, 178, 12, 209, 91, 243, 63}
+	defaultAddress := types.Address{
+		Object: &object.Object{
+			Kind:    object.UserAddress,
+			Version: types.AddressLastVersion,
+			Data:    []byte{0x77, 0x13, 0x86, 0x8f, 0xe5, 0x5a, 0xd1, 0xdb, 0x9c, 0x8, 0x30, 0x7c, 0x61, 0x5e, 0xdf, 0xc0, 0xc8, 0x3b, 0x5b, 0xd9, 0x88, 0xec, 0x2e, 0x3c, 0xe9, 0xe4, 0x1c, 0xf1, 0xf9, 0x4d, 0xc5, 0xd1},
+		},
+	}
+	defaultEncryptedPrivateKey := types.EncryptedPrivateKey{
+		Object: &object.Object{
+			Kind:    object.EncryptedPrivateKey,
+			Version: types.EncryptedPrivateKeyLastVersion,
+			Data:    []byte{2, 86, 133, 146, 82, 184, 193, 160, 120, 44, 198, 209, 69, 230, 83, 35, 36, 235, 18, 105, 74, 117, 228, 237, 112, 65, 32, 0, 250, 180, 199, 26, 40, 28, 76, 116, 162, 95, 0, 103, 172, 8, 41, 11, 240, 185, 188, 215, 56, 170, 246, 2, 14, 16, 27, 214, 137, 103, 89, 111, 85, 149, 191, 38, 2, 43, 8, 183, 149, 104, 64, 149, 10, 106, 102, 156, 242, 178, 254, 189, 135},
+		},
+	}
+	defaultPublicKey := types.PublicKey{
+		Object: &object.Object{
+			Kind:    object.PublicKey,
+			Version: types.PublicKeyLastVersion,
+			Data:    []byte{45, 150, 188, 218, 203, 190, 65, 56, 44, 162, 62, 82, 227, 210, 25, 108, 186, 101, 231, 161, 172, 210, 9, 223, 201, 92, 107, 50, 182, 161, 138, 147},
+		},
+	}
+	defaultNickname := "bonjour2"
+	defaultAccount, err := account.New(
 		uint8(account.LastVersion),
-		sampleNickname,
-		&types.Address{
-			Object: &object.Object{
-				Kind:    object.UserAddress,
-				Version: types.AddressLastVersion,
-				Data:    []byte{0x77, 0x13, 0x86, 0x8f, 0xe5, 0x5a, 0xd1, 0xdb, 0x9c, 0x8, 0x30, 0x7c, 0x61, 0x5e, 0xdf, 0xc0, 0xc8, 0x3b, 0x5b, 0xd9, 0x88, 0xec, 0x2e, 0x3c, 0xe9, 0xe4, 0x1c, 0xf1, 0xf9, 0x4d, 0xc5, 0xd1},
-			},
-		},
-		sampleSalt,
-		sampleNonce,
-		&types.EncryptedPrivateKey{
-			Object: &object.Object{
-				Kind:    object.EncryptedPrivateKey,
-				Version: types.EncryptedPrivateKeyLastVersion,
-				Data:    []byte{2, 86, 133, 146, 82, 184, 193, 160, 120, 44, 198, 209, 69, 230, 83, 35, 36, 235, 18, 105, 74, 117, 228, 237, 112, 65, 32, 0, 250, 180, 199, 26, 40, 28, 76, 116, 162, 95, 0, 103, 172, 8, 41, 11, 240, 185, 188, 215, 56, 170, 246, 2, 14, 16, 27, 214, 137, 103, 89, 111, 85, 149, 191, 38, 2, 43, 8, 183, 149, 104, 64, 149, 10, 106, 102, 156, 242, 178, 254, 189, 135},
-			},
-		},
-		&types.PublicKey{
-			Object: &object.Object{
-				Kind:    object.PublicKey,
-				Version: types.PublicKeyLastVersion,
-				Data:    []byte{45, 150, 188, 218, 203, 190, 65, 56, 44, 162, 62, 82, 227, 210, 25, 108, 186, 101, 231, 161, 172, 210, 9, 223, 201, 92, 107, 50, 182, 161, 138, 147},
-			},
-		},
+		defaultNickname,
+		&defaultAddress,
+		defaultSalt,
+		defaultNonce,
+		&defaultEncryptedPrivateKey,
+		&defaultPublicKey,
 	)
 	assert.NoError(t, err)
 
@@ -62,17 +65,17 @@ func TestWallet(t *testing.T) {
 	})
 
 	t.Run("Add Account", func(t *testing.T) {
-		err := w.AddAccount(sampleAccount, true)
+		err := w.AddAccount(defaultAccount, true)
 		assert.NoError(t, err)
 
 		assert.Equal(t, 1, w.GetAccountCount())
-		accountPath, err := w.AccountPath(sampleNickname)
+		accountPath, err := w.AccountPath(defaultNickname)
 		assert.NoError(t, err)
 		assert.FileExists(t, accountPath)
 	})
 
 	t.Run("Add Account: nickname not unique", func(t *testing.T) {
-		err := w.AddAccount(sampleAccount, true)
+		err := w.AddAccount(defaultAccount, true)
 		assert.Error(t, err)
 		assert.ErrorIs(t, err, ErrNicknameNotUnique)
 
@@ -83,29 +86,11 @@ func TestWallet(t *testing.T) {
 		sampleAccount, err := account.New(
 			uint8(account.LastVersion),
 			"bonjour3",
-			&types.Address{
-				Object: &object.Object{
-					Kind:    object.UserAddress,
-					Version: types.AddressLastVersion,
-					Data:    []byte{0x77, 0x13, 0x86, 0x8f, 0xe5, 0x5a, 0xd1, 0xdb, 0x9c, 0x8, 0x30, 0x7c, 0x61, 0x5e, 0xdf, 0xc0, 0xc8, 0x3b, 0x5b, 0xd9, 0x88, 0xec, 0x2e, 0x3c, 0xe9, 0xe4, 0x1c, 0xf1, 0xf9, 0x4d, 0xc5, 0xd1},
-				},
-			},
-			sampleSalt,
-			sampleNonce,
-			&types.EncryptedPrivateKey{
-				Object: &object.Object{
-					Kind:    object.EncryptedPrivateKey,
-					Version: types.EncryptedPrivateKeyLastVersion,
-					Data:    []byte{2, 86, 133, 146, 82, 184, 193, 160, 120, 44, 198, 209, 69, 230, 83, 35, 36, 235, 18, 105, 74, 117, 228, 237, 112, 65, 32, 0, 250, 180, 199, 26, 40, 28, 76, 116, 162, 95, 0, 103, 172, 8, 41, 11, 240, 185, 188, 215, 56, 170, 246, 2, 14, 16, 27, 214, 137, 103, 89, 111, 85, 149, 191, 38, 2, 43, 8, 183, 149, 104, 64, 149, 10, 106, 102, 156, 242, 178, 254, 189, 135},
-				},
-			},
-			&types.PublicKey{
-				Object: &object.Object{
-					Kind:    object.PublicKey,
-					Version: types.PublicKeyLastVersion,
-					Data:    []byte{45, 150, 188, 218, 203, 190, 65, 56, 44, 162, 62, 82, 227, 210, 25, 108, 186, 101, 231, 161, 172, 210, 9, 223, 201, 92, 107, 50, 182, 161, 138, 147},
-				},
-			},
+			&defaultAddress,
+			defaultSalt,
+			defaultNonce,
+			&defaultEncryptedPrivateKey,
+			&defaultPublicKey,
 		)
 		assert.NoError(t, err)
 
@@ -115,26 +100,26 @@ func TestWallet(t *testing.T) {
 
 		assert.Equal(t, 1, w.GetAccountCount())
 
-		sampleAccount.Nickname = "bonjour2"
+		sampleAccount.Nickname = defaultNickname
 	})
 
 	t.Run("Get Account", func(t *testing.T) {
-		acc, err := w.GetAccount(sampleNickname)
+		acc, err := w.GetAccount(defaultNickname)
 		assert.NoError(t, err)
 		assert.NotNil(t, acc)
 		assert.Equal(t, uint8(1), acc.Version)
-		assert.Equal(t, sampleNickname, acc.Nickname)
-		assert.Equal(t, sampleSalt, acc.Salt)
-		assert.Equal(t, sampleNonce, acc.Nonce)
-		assert.Equal(t, sampleAccount.Address.Object.Data, acc.Address.Object.Data)
-		assert.Equal(t, sampleAccount.Address.Object.Version, acc.Address.Object.Version)
-		assert.Equal(t, sampleAccount.Address.Object.Kind, acc.Address.Object.Kind)
-		assert.Equal(t, sampleAccount.CipheredData.Object.Data, acc.CipheredData.Object.Data)
-		assert.Equal(t, sampleAccount.CipheredData.Object.Version, acc.CipheredData.Object.Version)
-		assert.Equal(t, sampleAccount.CipheredData.Object.Kind, acc.CipheredData.Object.Kind)
-		assert.Equal(t, sampleAccount.PublicKey.Object.Data, acc.PublicKey.Object.Data)
-		assert.Equal(t, sampleAccount.PublicKey.Object.Version, acc.PublicKey.Object.Version)
-		assert.Equal(t, sampleAccount.PublicKey.Object.Kind, acc.PublicKey.Object.Kind)
+		assert.Equal(t, defaultNickname, acc.Nickname)
+		assert.Equal(t, defaultSalt, acc.Salt)
+		assert.Equal(t, defaultNonce, acc.Nonce)
+		assert.Equal(t, defaultAccount.Address.Object.Data, acc.Address.Object.Data)
+		assert.Equal(t, defaultAccount.Address.Object.Version, acc.Address.Object.Version)
+		assert.Equal(t, defaultAccount.Address.Object.Kind, acc.Address.Object.Kind)
+		assert.Equal(t, defaultAccount.CipheredData.Object.Data, acc.CipheredData.Object.Data)
+		assert.Equal(t, defaultAccount.CipheredData.Object.Version, acc.CipheredData.Object.Version)
+		assert.Equal(t, defaultAccount.CipheredData.Object.Kind, acc.CipheredData.Object.Kind)
+		assert.Equal(t, defaultAccount.PublicKey.Object.Data, acc.PublicKey.Object.Data)
+		assert.Equal(t, defaultAccount.PublicKey.Object.Version, acc.PublicKey.Object.Version)
+		assert.Equal(t, defaultAccount.PublicKey.Object.Kind, acc.PublicKey.Object.Kind)
 	})
 
 	t.Run("Get Account: not found", func(t *testing.T) {
@@ -171,11 +156,11 @@ func TestWallet(t *testing.T) {
 	})
 
 	t.Run("Delete Account", func(t *testing.T) {
-		err := w.DeleteAccount(sampleNickname)
+		err := w.DeleteAccount(defaultNickname)
 		assert.NoError(t, err)
 		assert.Equal(t, 1, w.GetAccountCount())
 
-		accountPath, err := w.AccountPath(sampleNickname)
+		accountPath, err := w.AccountPath(defaultNickname)
 		assert.NoError(t, err)
 		assert.NoFileExists(t, accountPath)
 	})
