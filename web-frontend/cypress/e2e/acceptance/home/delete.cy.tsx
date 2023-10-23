a806deaf5155004ef76f22f564a9d06b88a8b6e8
pivi <paul.lartisant@icloud.com>
2023-10-23 17:11:42 +0200

    Add Delete Test Flow

--- web-frontend/cypress/e2e/acceptance/home/delete.cy.tsx
+++ web-frontend/cypress/e2e/acceptance/home/delete.cy.tsx
@@ -3,7 +3,7 @@
 import { Server } from 'miragejs';
 
 import { mockServer } from '../../../../src/mirage';
-// import { compareSnapshot } from '../../../compareSnapshot';
+import { compareSnapshot } from '../../../compareSnapshot';
 
 describe('E2E | Acceptance | Home', () => {
   let server: Server;
@@ -18,13 +18,15 @@ describe('E2E | Acceptance | Home', () => {
   });
 
   describe('delete', () => {
+    // accounts used for test
     let mockedAccounts;
 
     function mockAccounts() {
-      const mockedAccounts = server.createList('account', 2);
+      const mockedAccounts = server.createList('account', 1);
       mockedAccounts.forEach((account) => {
-        server.createList('asset', 3, { account });
+        server.createList('asset', 1, { account });
       });
+      // reference account
       const account = {
         nickname: 'Mario',
         address: 'AUHdadXyJZUeINwiUVMtXZXJRTFXtYdihRWitUcAJSBwAHgcKAjtxx',
@@ -39,13 +41,58 @@ describe('E2E | Acceptance | Home', () => {
     });
 
     it('should direct me to /nickname/home', () => {
-      const account = mockedAccounts.at(2);
+      // position of reference account in mockedAccounts
+      const account = mockedAccounts.at(1);
 
       cy.visit('/');
 
-      cy.get('[data-testid="account-2"]').click();
+      cy.get('[data-testid="account-1"]').click();
 
       cy.url().should('eq', `${baseUrl}/${account.nickname}/home`);
+
+      cy.get('[data-testid="side-menu"]').should('exist').and('be.visible');
+      compareSnapshot(cy, 'delete-home');
+    });
+
+    it('should access delete page', () => {
+      const account = mockedAccounts.at(1);
+
+      cy.visit('/');
+
+      cy.get('[data-testid="account-1"]').click();
+
+      cy.get('[data-testid="side-menu"]').click();
+
+      cy.get('[data-testid="side-menu-settings-icon"]')
+        .should('exist')
+        .and('be.visible')
+        .click();
+
+      cy.url().should('eq', `${baseUrl}/${account.nickname}/settings`);
+      compareSnapshot(cy, 'delete-settings-page');
+    });
+
+    it('should delete account', () => {
+      const account = mockedAccounts.at(1);
+
+      cy.visit('/');
+
+      cy.get('[data-testid="account-1"]').click();
+
+      cy.get('[data-testid="side-menu"]').click();
+
+      cy.get('[data-testid="side-menu-settings-icon"]').click();
+
+      cy.get('[data-testid="button"]')
+        .contains('Delete account')
+        .should('exist')
+        .and('be.visible')
+        .click();
+
+      cy.url().should('eq', `${baseUrl}/account-select`);
+
+      cy.get('[data-testid="account-1"]').should('not.exist');
+      compareSnapshot(cy, 'delete-account-select');
     });
   });
 });
