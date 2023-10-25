81102277612b3f805b3dbda2a5ea853a6db3bc51
pivi <paul.lartisant@icloud.com>
2023-10-25 12:03:28 +0200

    Add receive test flow

--- web-frontend/cypress/e2e/acceptance/home/receive.cy.tsx
+++ web-frontend/cypress/e2e/acceptance/home/receive.cy.tsx
@@ -3,7 +3,7 @@
 import { mockServer } from '../../../../src/mirage';
 import { AccountObject } from '@/models/AccountModel';
 
-describe('E2E | Acceptance | Account | Receive', () => {
+describe('E2E | Acceptance | Home | Receive', () => {
   let server: any;
   const baseUrl = Cypress.config().baseUrl;
 
@@ -15,7 +15,7 @@ describe('E2E | Acceptance | Account | Receive', () => {
     server.shutdown();
   });
 
-  describe('send', () => {
+  describe('receive', () => {
     let mockedAccounts;
 
     function mockAccounts() {
@@ -36,6 +36,13 @@ describe('E2E | Acceptance | Account | Receive', () => {
       mockedAccounts = mockAccounts();
     });
 
+    function navigateToReceivePage() {
+      cy.visit('/');
+
+      cy.get('[data-testid="account-2"]').click();
+      cy.get('[data-testid="receive-button"]').click();
+    }
+
     it('should land on receive page when receive CTA is clicked', () => {
       const account = mockedAccounts.at(2);
 
@@ -47,6 +54,66 @@ describe('E2E | Acceptance | Account | Receive', () => {
 
       cy.get('[data-testid="send-coins"]').should('not.be.visible');
       cy.get('[data-testid="receive-coins"]').should('be.visible');
+    });
+
+    it('clipboard field should contain user address', () => {
+      const account = mockedAccounts.at(2);
+
+      navigateToReceivePage();
+      cy.get('[data-testid="clipboard-field"]').contains(account.address);
+    });
+
+    it('should copy from clipboard', () => {
+      const account = mockedAccounts.at(2);
+      navigateToReceivePage();
+      cy.on('window:confirm', () => true);
+
+      cy.window().then((win) => {
+        cy.stub(win, 'prompt').returns(win.prompt).as('copyToClipboardPrompt');
+      });
+
+      cy.get('[data-testid="clipboard-field"]').click();
+      cy.get('@copyToClipboardPrompt').should('be.called');
+
+      cy.get('@copyToClipboardPrompt').should((prompt) => {
+        expect(prompt.args[0][1]).to.equal(account.address);
+      });
+    });
+
+    it('should generate and copy link', () => {
+      const amount = 5000;
+      const account = mockedAccounts.at(2);
+
+      const generatedLink = `http://localhost:8080/send-redirect/?to=${account.address}&amount=${amount}`;
+
+      navigateToReceivePage();
+      cy.get('[data-testid="button"]')
+        .should('exist')
+        .contains('Generate link')
+        .click();
+
+      cy.get('[data-testid="popup-modal"]').should('exist');
+      cy.get('[data-testid="amount-to-send"]')
+
+        .should('have.attr', 'placeholder', 'Amount to ask')
+        .type(amount);
+      cy.get('[data-testid="clipboard-field"]').should('contain', '');
+      cy.get('[data-testid="generate-link-button"]').click();
+
+      cy.on('window:confirm', () => true);
+
+      cy.window().then((win) => {
+        cy.stub(win, 'prompt').returns(win.prompt).as('copyToClipboardPrompt');
+      });
+
+      cy.get('[data-testid="clipboard-link"]')
+        .should('contain', generatedLink)
+        .click();
+      cy.get('@copyToClipboardPrompt').should('be.called');
+
+      cy.get('@copyToClipboardPrompt').should((prompt) => {
+        expect(prompt.args[0][1]).to.equal(generatedLink);
+      });
     });
   });
 });
