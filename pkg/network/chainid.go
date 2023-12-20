4a9a6da1b12b47a4441f3b7a623820ea541f9f01
Nathan Seva <thykof@protonmail.ch>
2023-12-20 13:35:53 -0500

    improve error handling

--- pkg/network/chainid.go
+++ pkg/network/chainid.go
@@ -6,7 +6,9 @@ import (
 	"github.com/massalabs/station/pkg/node"
 )
 
-var ErrChainIDNotInStatus = errors.New("chain id not in status")
+var (
+	ErrChainIDNotInStatus = errors.New("chain id not in status")
+)
 
 func getChainID() (uint64, error) {
 	client, err := NewMassaClient()
