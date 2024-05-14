a8dee70398f62a29a29d76ea55d8634bc7f6e78c
Phoebe Lartisant <phoebelartisant@proton.me>
2024-05-14 17:23:36 +0200

    Use network chainID for contracts

--- web-frontend/src/custom/useMNS.tsx
+++ web-frontend/src/custom/useMNS.tsx
@@ -1,21 +1,24 @@
 import { useCallback, useState } from 'react';
 
-import { Args, ICallData } from '@massalabs/massa-web3';
+import { Args, ICallData, MAINNET_CHAIN_ID } from '@massalabs/massa-web3';
 import { bytesToStr } from '@massalabs/web3-utils';
 
 import { usePrepareScCall } from './usePrepareScCall';
-import { contracts } from '@/utils/const';
+import { contracts, networks } from '@/utils/const';
 
 export function useMNS() {
   const [address, setAddress] = useState<string>('');
   const [mns, setMns] = useState<string>('');
 
-  const { client, account } = usePrepareScCall();
+  const { client, account, chainId } = usePrepareScCall();
 
   const accountAddress = account?.address() ?? '';
 
+  const currentNetwork =
+    chainId === MAINNET_CHAIN_ID ? networks.mainnet : networks.buildnet;
+
   const reverseResolveCallData = {
-    targetAddress: contracts.buildnet.MNSContract,
+    targetAddress: contracts[currentNetwork].mnsContract,
     targetFunction: 'dnsReverseResolve',
     parameter: new Args().addString(accountAddress).serialize(),
   } as ICallData;
@@ -37,7 +40,7 @@ export function useMNS() {
     if (!client) return;
 
     const resolveCallData = {
-      targetAddress: contracts.buildnet.MNSContract,
+      targetAddress: contracts[currentNetwork].mnsContract,
       targetFunction: 'dnsResolve',
       parameter: new Args().addString(domain).serialize(),
     } as ICallData;
