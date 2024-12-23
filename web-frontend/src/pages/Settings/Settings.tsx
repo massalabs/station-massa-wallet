773d1493cd1b2923b600e88616ac9806f30804a2
thomas-senechal <thomas.senechal@pm.me>
2024-12-23 12:05:59 +0100

    Add SettingsAutoSign component for managing auto-sign rules

--- web-frontend/src/pages/Settings/Settings.tsx
+++ web-frontend/src/pages/Settings/Settings.tsx
@@ -1,7 +1,8 @@
-import { Button, Identicon, Input } from '@massalabs/react-ui-kit';
+import { Button, Identicon, Input, Tabs } from '@massalabs/react-ui-kit';
 import { FiEdit } from 'react-icons/fi';
 import { useNavigate, useParams } from 'react-router-dom';
 
+import SettingsAutoSign from './SettingsAutoSign';
 import { SettingsOption } from './SettingsOption';
 import Intl from '@/i18n/i18n';
 import { WalletLayout, MenuItem } from '@/layouts/WalletLayout/WalletLayout';
@@ -11,34 +12,53 @@ export default function Settings() {
   const navigate = useNavigate();
   const { nickname } = useParams();
 
+  const tabs = [
+    {
+      label: `${nickname} settings`,
+      content: (
+        <div className="flex flex-col justify-center items-center gap-9">
+          <div className="bg-secondary rounded-2xl w-full max-w-2xl p-10">
+            <p className="mas-body text-f-primary pb-5">
+              {Intl.t('settings.title-profile')}
+            </p>
+            <div className="pb-7">
+              <Identicon
+                username={nickname || 'username'}
+                size={120}
+                className="bg-primary rounded-full"
+              />
+            </div>
+            <div className="pb-5">
+              <Input
+                value={nickname || 'username'}
+                icon={<FiEdit />}
+                onClickIcon={() =>
+                  navigate(routeFor(`${nickname}/settings/update`))
+                }
+                disabled
+              />
+            </div>
+            <Button disabled>{Intl.t('settings.buttons.update')}</Button>
+          </div>
+          <SettingsOption nickname={nickname || 'username'} />
+        </div>
+      ),
+    },
+    {
+      label: 'Global settings',
+      content: (
+        <div className="flex flex-col justify-center items-center gap-9">
+          <div className="bg-secondary rounded-2xl w-full max-w-2xl p-10">
+            <SettingsAutoSign nickname={nickname || 'username'} />
+          </div>
+        </div>
+      ),
+    },
+  ];
+
   return (
     <WalletLayout menuItem={MenuItem.Settings}>
-      <div className="flex flex-col justify-center items-center gap-9 w-1/2">
-        <div className="bg-secondary rounded-2xl w-full max-w-2xl p-10">
-          <p className="mas-body text-f-primary pb-5">
-            {Intl.t('settings.title-profile')}
-          </p>
-          <div className="pb-7">
-            <Identicon
-              username={nickname || 'username'}
-              size={120}
-              className="bg-primary rounded-full"
-            />
-          </div>
-          <div className="pb-5">
-            <Input
-              value={nickname || 'username'}
-              icon={<FiEdit />}
-              onClickIcon={() =>
-                navigate(routeFor(`${nickname}/settings/update`))
-              }
-              disabled
-            />
-          </div>
-          <Button disabled>{Intl.t('settings.buttons.update')}</Button>
-        </div>
-        <SettingsOption nickname={nickname || 'username'} />
-      </div>
+      <Tabs tabsConfig={tabs} />
     </WalletLayout>
   );
 }
