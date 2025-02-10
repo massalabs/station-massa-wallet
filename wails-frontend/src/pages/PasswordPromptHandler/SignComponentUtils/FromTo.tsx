0ba52fd529bc940f24834e0b8a67b5b1946920c5
Andrei <34773578+fleandrei@users.noreply.github.com>
2025-02-10 18:52:50 +0100

    display contract address on wails and web asset (#991)
    * display contract address on wails and web asset
    
    * fix Clipboard import syntaxe
    
    * make npm install to update package-lock.json

--- wails-frontend/src/pages/PasswordPromptHandler/SignComponentUtils/FromTo.tsx
+++ wails-frontend/src/pages/PasswordPromptHandler/SignComponentUtils/FromTo.tsx
@@ -1,7 +1,6 @@
-import { maskAddress, maskNickname } from '@massalabs/react-ui-kit';
+import { maskAddress, maskNickname, Clipboard } from '@massalabs/react-ui-kit';
 import { FiArrowRight } from 'react-icons/fi';
 
-import { CopyClip } from '../components/clipBoardCopy';
 import Intl from '@/i18n/i18n';
 
 interface FromToProps {
@@ -23,19 +22,23 @@ export function FromTo(props: FromToProps) {
 
   return (
     <div className="flex w-full items-center justify-between">
-      <div className="flex flex-col">
+      <div className="flex flex-col gap-y-2">
         <div className="flex gap-2">
           <p className="mas-menu-active">
             {Intl.t('password-prompt.sign.from')}
           </p>
           <p className="mas-menu-default">{maskNickname(nickname)}</p>
         </div>
-        <p className="mas-caption">{maskAddress(walletAddress)}</p>
+        <Clipboard
+          customClass="h-8 w-10"
+          rawContent={walletAddress}
+          displayedContent={maskAddress(walletAddress)}
+        />
       </div>
       <div className="h-8 w-8 rounded-full flex items-center justify-center bg-neutral">
         <FiArrowRight size={24} className="text-primary" />
       </div>
-      <div className="flex flex-col">
+      <div className="flex flex-col gap-y-2">
         <div className="flex gap-2">
           <p className="mas-menu-active">{Intl.t(label)}</p>
           {recipientNickname ? (
@@ -44,12 +47,11 @@ export function FromTo(props: FromToProps) {
             </p>
           ) : null}
         </div>
-        <div className="flex items-center">
-          <p className="mas-caption">{maskAddress(recipientAddress)}</p>
-          <div className="ml-2">
-            <CopyClip data={recipientAddress} />
-          </div>
-        </div>
+        <Clipboard
+          customClass="h-8 w-10"
+          rawContent={recipientAddress}
+          displayedContent={maskAddress(recipientAddress)}
+        />
       </div>
     </div>
   );
